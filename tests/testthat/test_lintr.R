6155ceecdbed2862fe2c1a9594d8a49cd3dc0413
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-08 22:54:04 +0100

    Improving

---
+++ tests/testthat/test_lintr.R
@@ -0,0 +1,8 @@
+test_that("Package must pass lintr", {
+  if (requireNamespace("lintr", quietly = TRUE)) {
+    context("lints")
+    test_that("Package Style", {
+      lintr::expect_lint_free()
+    })
+  }
+})
