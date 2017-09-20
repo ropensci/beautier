a5210fbc40b95ca4084cf063d1b8351fdc2d2d8a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-20 14:58:19 +0200

    Simplified beastscriptr code

--- R/beastscriptr.R
+++ R/beastscriptr.R
@@ -248,53 +248,11 @@ beast_scriptr <- function(
   text <- c(text, "        </distribution>")
   text <- c(text, "    </distribution>")
   text <- c(text, "")
-  text <- c(text, paste("    <operator id=\"treeScaler.t:", filename_base,
-    "\" spec=\"ScaleOperator\" scaleFactor=\"0.5\" tree=\"@Tree.t:",
-    filename_base, "\" weight=\"3.0\"/>", sep = ""))                            # nolint (as this is no absolute path)
-  text <- c(text, "")
-  text <- c(text, paste("    <operator id=\"treeRootScaler.t:", filename_base,
-    "\" spec=\"ScaleOperator\" rootOnly=\"true\" scaleFactor=\"0.5\" ",
-    "tree=\"@Tree.t:", filename_base, "\" weight=\"3.0\"/>", sep = ""))          # nolint (as this is no absolute path)
-  text <- c(text, "")
-  text <- c(text, paste("    <operator id=\"UniformOperator.t:", filename_base,
-    "\" spec=\"Uniform\" tree=\"@Tree.t:", filename_base,
-    "\" weight=\"30.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
-  text <- c(text, "")
-  if (fixed_crown_age == FALSE) {
-    text <- c(text, paste("    <operator id=\"SubtreeSlide.t:", filename_base,
-      "\" spec=\"SubtreeSlide\" tree=\"@Tree.t:", filename_base,
-      "\" weight=\"15.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
-    text <- c(text, "")
-    text <- c(text, paste("    <operator id=\"narrow.t:", filename_base,
-      "\" spec=\"Exchange\" tree=\"@Tree.t:", filename_base,
-      "\" weight=\"15.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
-    text <- c(text, "")
-    text <- c(text, paste("    <operator id=\"wide.t:", filename_base,
-      "\" spec=\"Exchange\" isNarrow=\"false\" tree=\"@Tree.t:", filename_base,
-      "\" weight=\"3.0\"/>", sep = ""))                                           # nolint (as this is no absolute path)
-    text <- c(text, "")
-    text <- c(text, paste("    <operator id=\"WilsonBalding.t:", filename_base,
-      "\" spec=\"WilsonBalding\" tree=\"@Tree.t:", filename_base,
-      "\" weight=\"3.0\"/>", sep = ""))                                           # nolint (as this is no absolute path)
-    text <- c(text, "")
-  }
-
-  if (tree_prior == "birth_death") {
-    text <- c(text, paste("    <operator id=\"BirthRateScaler.t:",
-      filename_base, "\" spec=\"ScaleOperator\" parameter=\"@birthRate2.t:",
-      filename_base, "\" scaleFactor=\"0.75\" weight=\"3.0\"/>", sep = ""))     # nolint (as this is no absolute path)
-    text <- c(text, "")
-    text <- c(text, paste("    <operator id=\"DeathRateScaler.t:",
-      filename_base,
-      "\" spec=\"ScaleOperator\" parameter=\"@relativeDeathRate2.t:",
-      filename_base, "\" scaleFactor=\"0.75\" weight=\"3.0\"/>", sep = ""))     # nolint (as this is no absolute path)
-  } else {
-    testit::assert(tree_prior == "coalescent_constant_population")
-    text <- c(text, paste("    <operator id=\"PopSizeScaler.t:",
-      filename_base, "\" parameter=\"@popSize.t:", filename_base,
-      "\" scaleFactor=\"0.75\" spec=\"ScaleOperator\" weight=\"3.0\"/>",        # nolint (as this is no absolute path)
-      sep = ""))
-  }
+  # Operators here
+  text <- c(text, beast_scriptr_operators(
+    filename_base = filename_base,
+    tree_prior = tree_prior,
+    fixed_crown_age = fixed_crown_age))
 
   text <- c(text, "")
   text <- c(text, paste("    <logger id=\"tracelog\" fileName=\"",
@@ -352,4 +310,72 @@ beast_scriptr <- function(
   my_file <- file(output_xml_filename)
   writeLines(text, my_file)
   close(my_file)
+}
+
+
+
+#' Creates the operators section of a BEAST2 XML parameter file
+#' @param filename_base base of the filenames
+#' @param tree_prior The tree prior, can be 'birth_death' or
+#'   'coalescent_constant_population'
+#' @param fixed_crown_age determines if the phylogeny its crown age is
+#'   fixed. If FALSE, crown age is estimated by BEAST2. If TRUE,
+#'   the crown age is fixed to the crown age
+#'   of the initial phylogeny.
+#' @export
+beast_scriptr_operators <- function(
+  filename_base,
+  tree_prior,
+  fixed_crown_age
+) {
+
+  text <- NULL
+  text <- c(text, paste("    <operator id=\"treeScaler.t:", filename_base,
+    "\" spec=\"ScaleOperator\" scaleFactor=\"0.5\" tree=\"@Tree.t:",
+    filename_base, "\" weight=\"3.0\"/>", sep = ""))                            # nolint (as this is no absolute path)
+  text <- c(text, "")
+  text <- c(text, paste("    <operator id=\"treeRootScaler.t:", filename_base,
+    "\" spec=\"ScaleOperator\" rootOnly=\"true\" scaleFactor=\"0.5\" ",
+    "tree=\"@Tree.t:", filename_base, "\" weight=\"3.0\"/>", sep = ""))          # nolint (as this is no absolute path)
+  text <- c(text, "")
+  text <- c(text, paste("    <operator id=\"UniformOperator.t:", filename_base,
+    "\" spec=\"Uniform\" tree=\"@Tree.t:", filename_base,
+    "\" weight=\"30.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
+  text <- c(text, "")
+  if (fixed_crown_age == FALSE) {
+    text <- c(text, paste("    <operator id=\"SubtreeSlide.t:", filename_base,
+      "\" spec=\"SubtreeSlide\" tree=\"@Tree.t:", filename_base,
+      "\" weight=\"15.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
+    text <- c(text, "")
+    text <- c(text, paste("    <operator id=\"narrow.t:", filename_base,
+      "\" spec=\"Exchange\" tree=\"@Tree.t:", filename_base,
+      "\" weight=\"15.0\"/>", sep = ""))                                          # nolint (as this is no absolute path)
+    text <- c(text, "")
+    text <- c(text, paste("    <operator id=\"wide.t:", filename_base,
+      "\" spec=\"Exchange\" isNarrow=\"false\" tree=\"@Tree.t:", filename_base,
+      "\" weight=\"3.0\"/>", sep = ""))                                           # nolint (as this is no absolute path)
+    text <- c(text, "")
+    text <- c(text, paste("    <operator id=\"WilsonBalding.t:", filename_base,
+      "\" spec=\"WilsonBalding\" tree=\"@Tree.t:", filename_base,
+      "\" weight=\"3.0\"/>", sep = ""))                                           # nolint (as this is no absolute path)
+    text <- c(text, "")
+  }
+
+  if (tree_prior == "birth_death") {
+    text <- c(text, paste("    <operator id=\"BirthRateScaler.t:",
+      filename_base, "\" spec=\"ScaleOperator\" parameter=\"@birthRate2.t:",
+      filename_base, "\" scaleFactor=\"0.75\" weight=\"3.0\"/>", sep = ""))     # nolint (as this is no absolute path)
+    text <- c(text, "")
+    text <- c(text, paste("    <operator id=\"DeathRateScaler.t:",
+      filename_base,
+      "\" spec=\"ScaleOperator\" parameter=\"@relativeDeathRate2.t:",
+      filename_base, "\" scaleFactor=\"0.75\" weight=\"3.0\"/>", sep = ""))     # nolint (as this is no absolute path)
+  } else {
+    testit::assert(tree_prior == "coalescent_constant_population")
+    text <- c(text, paste("    <operator id=\"PopSizeScaler.t:",
+      filename_base, "\" parameter=\"@popSize.t:", filename_base,
+      "\" scaleFactor=\"0.75\" spec=\"ScaleOperator\" weight=\"3.0\"/>",        # nolint (as this is no absolute path)
+      sep = ""))
+  }
+  text
 }
