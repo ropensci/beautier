ea87af2a55b2cefa6099b951c6a4943df34e1ad7
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-12 13:39:41 +0200

    Added is_ccp_tree_prior

---
+++ R/is_ccp_tree_prior.R
@@ -0,0 +1,10 @@
+#' Determine if the object is a valid constant coalescence population tree prior
+#' @param x an object, to be determined if it is a valid constant coalescence population tree prior
+#' @return TRUE if x is a valid constant coalescence population tree prior, FALSE otherwise
+#' @author Richel Bilderbeek
+#' @export
+is_ccp_tree_prior <- function(
+  x
+) {
+  return("name" %in% names(x) && x$name == "coalescent_constant_population")
+}
---
+++ tests/testthat/test-is_ccp_tree_prior.R
@@ -0,0 +1,15 @@
+context("is_ccp_tree_prior")
+
+test_that("usage", {
+  testthat::expect_true(
+    is_ccp_tree_prior(
+      create_tree_prior(name = "coalescent_constant_population")
+    )
+  )
+
+  testthat::expect_false(
+    is_ccp_tree_prior(
+      "nonsense"
+    )
+  )
+})
