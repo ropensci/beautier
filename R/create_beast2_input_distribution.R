a04bf2e326dd59ddb88c5bb0558445d80b675545
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-18 15:16:00 +0200

    Yule 71/111

--- R/create_beast2_input_distribution.R
+++ R/create_beast2_input_distribution.R
@@ -18,6 +18,7 @@ create_beast2_input_distribution <- function(
     text <- c(text, paste0("            <prior id=\"YuleBirthRatePrior.t:", ids, "\" name=\"distribution\" x=\"@birthRate.t:", ids, "\">"))
     text <- c(text, paste0("                <Uniform id=\"Uniform.1\" name=\"distr\" upper=\"Infinity\"/>"))
     text <- c(text, paste0("            </prior>"))
+    text <- c(text, "        </distribution>")
   } else if (is_bd_tree_prior(tree_priors)) {
     text <- c(text, paste0("            <distribution id=\"BirthDeath.t:", ids, "\" spec=\"beast.evolution.speciation.BirthDeathGernhard08Model\" birthDiffRate=\"@BDBirthRate.t:", ids, "\" relativeDeathRate=\"@BDDeathRate.t:", ids, "\" tree=\"@Tree.t:", ids, "\"/>"))
     text <- c(text, paste0("            <prior id=\"BirthRatePrior.t:", ids, "\" name=\"distribution\" x=\"@BDBirthRate.t:", ids, "\">"))
