32b730a0bba78ba8c69bd36a17e21efec9152825
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-19 14:45:56 +0200

    relaxed_clock_log_normal_2_4 41/132

--- R/get_clock_model_names.R
+++ R/get_clock_model_names.R
@@ -2,5 +2,5 @@
 #' @return the clock model names
 #' @export
 get_clock_model_names <- function() {
-  return(c("strict"))
+  return(c("relaxed_log_normal", "strict"))
 }
---
+++ R/is_clock_model_name.R
@@ -0,0 +1,7 @@
+#' Determines if the name is a valid clock model name
+#' @param name the name to be tested
+#' @return TRUE if the name is a valid clock_model name, FALSE otherwise
+#' @export
+is_clock_model_name <- function(name) {
+  return(name %in% beastscriptr::get_clock_model_names())
+}
