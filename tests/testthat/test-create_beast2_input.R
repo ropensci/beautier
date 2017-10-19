46d7e5d9888ae801220c005a3c15282b32157e0e
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-19 12:55:32 +0200

    GTR 41/170

--- tests/testthat/test-create_beast2_input.R
+++ tests/testthat/test-create_beast2_input.R
@@ -155,26 +155,21 @@ test_that("Check that gtr_2_4.xml is reproduced", {
 
   skip("WIP")
 
-  input_fasta_filenames <- beastscriptr::get_input_fasta_filename()
-
-  # Input file must be found
-  testthat::expect_true(file.exists(input_fasta_filenames))
-
   created_lines <- beastscriptr::create_beast2_input(
-    input_fasta_filenames = input_fasta_filenames,
+    input_fasta_filenames = beastscriptr::get_input_fasta_filename(),
     site_models = create_site_model(name = "GTR")
   )
 
   expected_lines <- readLines(system.file("extdata", "gtr_2_4.xml", package = "beastscriptr"))
 
-  # write.csv(created_lines, "~/created.csv")
-  # write.csv(expected_lines, "~/expected.csv")
-  # for (i in 1:min(length(expected_lines), length(created_lines))) {
-  #   testthat::expect_equal(
-  #     expected_lines[i], created_lines[i]
-  #   )
-  #   print(paste0(i, " / ", length(expected_lines)))
-  # }
+  write.csv(created_lines, "~/created.csv")
+  write.csv(expected_lines, "~/expected.csv")
+  for (i in 1:min(length(expected_lines), length(created_lines))) {
+    testthat::expect_equal(
+      expected_lines[i], created_lines[i]
+    )
+    print(paste0(i, " / ", length(expected_lines)))
+  }
 
   testthat::expect_identical(created_lines, expected_lines)
 })
