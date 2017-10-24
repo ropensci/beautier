bfd4ee1834631bffddfc26601551bde9d85925f4
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-24 13:07:50 +0200

    Use existing function only

--- tests/testthat/test-is_relaxed_log_normal_clock_model.R
+++ tests/testthat/test-is_relaxed_log_normal_clock_model.R
@@ -1,14 +1,14 @@
-context("is_relaxed_log_normal_clock_model")
+context("is_rln_clock_model")
 
 test_that("use", {
 
   testthat::expect_true(
-    is_relaxed_log_normal_clock_model(
+    is_rln_clock_model(
       create_clock_model(name = "relaxed_log_normal")
     )
   )
   testthat::expect_false(
-    is_relaxed_log_normal_clock_model(
+    is_rln_clock_model(
       create_clock_model(name = "strict")
     )
   )
