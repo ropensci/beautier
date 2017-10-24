e33f2ac9281bc3306072910fb8b905022ff6075a
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-24 11:47:25 +0200

    @lintr-bot

--- R/create_beast2_input_loggers.R
+++ R/create_beast2_input_loggers.R
@@ -46,32 +46,41 @@ create_beast2_input_loggers <- function( # nolint keep long function name, as it
     text <- c(text, paste0("        <log idref=\"CoalescentConstant.t:",
       ids, "\"/>"))
   } else if (is_cbs_tree_prior(tree_priors)) {
-    text <- c(text, paste0("        <log idref=\"BayesianSkyline.t:", ids, "\"/>"))
-    text <- c(text, paste0("        <log idref=\"bPopSizes.t:", ids, "\"/>"))
-    text <- c(text, paste0("        <log idref=\"bGroupSizes.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"BayesianSkyline.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"bPopSizes.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"bGroupSizes.t:", ids, "\"/>"))
   }
 
   if (is_hky_site_model(site_models)) {
     text <- c(text, paste0("        <log idref=\"kappa.s:", ids, "\"/>"))
-    text <- c(text, paste0("        <log idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
   } else if (is_tn93_site_model(site_models)) {
     text <- c(text, paste0("        <log idref=\"kappa1.s:", ids, "\"/>"))
     text <- c(text, paste0("        <log idref=\"kappa2.s:", ids, "\"/>"))
-    text <- c(text, paste0("        <log idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
   } else if (is_gtr_site_model(site_models)) {
     text <- c(text, paste0("        <log idref=\"rateAC.s:", ids, "\"/>"))
     text <- c(text, paste0("        <log idref=\"rateAG.s:", ids, "\"/>"))
     text <- c(text, paste0("        <log idref=\"rateAT.s:", ids, "\"/>"))
     text <- c(text, paste0("        <log idref=\"rateCG.s:", ids, "\"/>"))
     text <- c(text, paste0("        <log idref=\"rateGT.s:", ids, "\"/>"))
-    text <- c(text, paste0("        <log idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
 
   }
 
   # Clock models
   if (is_relaxed_log_normal_clock_model(clock_models)) {
     text <- c(text, paste0("        <log idref=\"ucldStdev.c:", ids, "\"/>"))
-    text <- c(text, paste0("        <log id=\"rate.c:", ids, "\" spec=\"beast.evolution.branchratemodel.RateStatistic\" branchratemodel=\"@RelaxedClock.c:", ids, "\" tree=\"@Tree.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log id=\"rate.c:", ids, "\" ",
+      "spec=\"beast.evolution.branchratemodel.RateStatistic\" ",
+      "branchratemodel=\"@RelaxedClock.c:", ids, "\" ",
+      "tree=\"@Tree.t:", ids, "\"/>"))
   }
 
   text <- c(text, "    </logger>")
@@ -84,13 +93,21 @@ create_beast2_input_loggers <- function( # nolint keep long function name, as it
   text <- c(text, "        <log idref=\"prior\"/>")
   text <- c(text, "    </logger>")
   text <- c(text, "")
-  text <- c(text, paste0("    <logger id=\"treelog.t:", ids, "\" fileName=\"$(tree).trees\" logEvery=\"1000\" mode=\"tree\">"))
+  text <- c(text, paste0("    <logger id=\"treelog.t:", ids, "\" ",
+    "fileName=\"$(tree).trees\" logEvery=\"1000\" mode=\"tree\">"))
 
   # Clock models
   if (is_strict_clock_model(clock_models)) {
-    text <- c(text, paste0("        <log id=\"TreeWithMetaDataLogger.t:", ids, "\" spec=\"beast.evolution.tree.TreeWithMetaDataLogger\" ", "tree=\"@Tree.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "id=\"TreeWithMetaDataLogger.t:", ids, "\" ",
+      "spec=\"beast.evolution.tree.TreeWithMetaDataLogger\" ",
+      "tree=\"@Tree.t:", ids, "\"/>"))
   } else if (is_relaxed_log_normal_clock_model(clock_models)) {
-    text <- c(text, paste0("        <log id=\"TreeWithMetaDataLogger.t:", ids, "\" spec=\"beast.evolution.tree.TreeWithMetaDataLogger\" branchratemodel=\"@RelaxedClock.c:", ids, "\" tree=\"@Tree.t:", ids, "\"/>"))
+    text <- c(text, paste0("        <log ",
+      "id=\"TreeWithMetaDataLogger.t:", ids, "\" ",
+      "spec=\"beast.evolution.tree.TreeWithMetaDataLogger\" ",
+      "branchratemodel=\"@RelaxedClock.c:", ids, "\" ",
+      "tree=\"@Tree.t:", ids, "\"/>"))
   }
 
   text <- c(text, "    </logger>")
