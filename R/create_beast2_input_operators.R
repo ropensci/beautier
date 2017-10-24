110df210da928f5509a94f75bed04a68f2c2b14b
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-24 11:53:57 +0200

    @lintr-bot

--- R/create_beast2_input_operators.R
+++ R/create_beast2_input_operators.R
@@ -35,75 +35,106 @@ create_beast2_input_operators <- function(
   text <- NULL
 
   if (is_yule_tree_prior(tree_priors)) {
-    text <- c(text, paste0("    <operator id=\"YuleBirthRateScaler.t:", ids, "\" spec=\"ScaleOperator\" parameter=\"@birthRate.t:", ids, "\" scaleFactor=\"0.75\" weight=\"3.0\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"YuleBirthRateScaler.t:", ids, "\" spec=\"ScaleOperator\" ",
+      "parameter=\"@birthRate.t:", ids, "\" scaleFactor=\"0.75\" ",
+      "weight=\"3.0\"/>"))
     text <- c(text, "")
   }
 
   if (fixed_crown_age == FALSE) {
-    text <- c(text, paste0("    <operator id=\"", operator_id_pre, "TreeScaler.t:", ids,
-      "\" spec=\"ScaleOperator\" scaleFactor=\"0.5\" tree=\"@Tree.t:",
+    text <- c(text, paste0("    <operator ",
+      "id=\"", operator_id_pre, "TreeScaler.t:", ids, "\" ",
+      "spec=\"ScaleOperator\" scaleFactor=\"0.5\" tree=\"@Tree.t:",
       ids, "\" weight=\"3.0\"/>"))
     text <- c(text, "")
   }
   if (fixed_crown_age == FALSE) {
-    text <- c(text, paste0("    <operator id=\"", operator_id_pre, "TreeRootScaler.t:",
-      ids,
-      "\" spec=\"ScaleOperator\" rootOnly=\"true\" scaleFactor=\"0.5\" ",
+    text <- c(text, paste0("    <operator ",
+      "id=\"", operator_id_pre, "TreeRootScaler.t:", ids, "\" ",
+      "spec=\"ScaleOperator\" rootOnly=\"true\" scaleFactor=\"0.5\" ",
       "tree=\"@Tree.t:", ids, "\" weight=\"3.0\"/>"))
     text <- c(text, "")
   }
-  text <- c(text, paste0("    <operator id=\"", operator_id_pre, "UniformOperator.t:", ids,
-    "\" spec=\"Uniform\" tree=\"@Tree.t:", ids,
-    "\" weight=\"30.0\"/>"))
+  text <- c(text, paste0("    <operator ",
+    "id=\"", operator_id_pre, "UniformOperator.t:", ids, "\" spec=\"Uniform\" ",
+    "tree=\"@Tree.t:", ids, "\" weight=\"30.0\"/>"))
   text <- c(text, "")
   if (fixed_crown_age == FALSE) {
-    text <- c(text, paste0("    <operator id=\"", operator_id_pre, "SubtreeSlide.t:", ids,
-      "\" spec=\"SubtreeSlide\" tree=\"@Tree.t:", ids,
-      "\" weight=\"15.0\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"", operator_id_pre, "SubtreeSlide.t:", ids, "\" ",
+      "spec=\"SubtreeSlide\" tree=\"@Tree.t:", ids, "\" weight=\"15.0\"/>"))
     text <- c(text, "")
   }
 
-  text <- c(text, paste0("    <operator id=\"", operator_id_pre, "Narrow.t:", ids, "\" spec=\"Exchange\" tree=\"@Tree.t:", ids,
-    "\" weight=\"15.0\"/>"))
+  text <- c(text, paste0("    <operator ",
+    "id=\"", operator_id_pre, "Narrow.t:", ids, "\" spec=\"Exchange\" ",
+    "tree=\"@Tree.t:", ids, "\" weight=\"15.0\"/>"))
   text <- c(text, "")
   text <- c(text, paste0("    <operator id=\"", operator_id_pre, "Wide.t:", ids,
     "\" spec=\"Exchange\" isNarrow=\"false\" tree=\"@Tree.t:", ids,
     "\" weight=\"3.0\"/>"))
   text <- c(text, "")
-  text <- c(text, paste0("    <operator id=\"", operator_id_pre, "WilsonBalding.t:", ids,
+  text <- c(text, paste0("    <operator ",
+    "id=\"", operator_id_pre, "WilsonBalding.t:", ids,
     "\" spec=\"WilsonBalding\" tree=\"@Tree.t:", ids,
     "\" weight=\"3.0\"/>"))
 
   if (is_hky_site_model(site_models)) {
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"KappaScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@kappa.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"KappaScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@kappa.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" delta=\"0.01\" weight=\"0.1\">"))
-    text <- c(text, paste0("        <parameter idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" ",
+      "delta=\"0.01\" weight=\"0.1\">"))
+    text <- c(text, paste0("        <parameter ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
     text <- c(text, paste0("    </operator>"))
   } else if (is_tn93_site_model(site_models)) {
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"kappa1Scaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@kappa1.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"kappa1Scaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@kappa1.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"kappa2Scaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@kappa2.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"kappa2Scaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@kappa2.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" delta=\"0.01\" weight=\"0.1\">"))
-    text <- c(text, paste0("        <parameter idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" ",
+      "delta=\"0.01\" weight=\"0.1\">"))
+    text <- c(text, paste0("        <parameter ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
     text <- c(text, paste0("    </operator>"))
   } else if (is_gtr_site_model(site_models)) {
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"RateACScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@rateAC.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"RateACScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@rateAC.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"RateAGScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@rateAG.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"RateAGScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@rateAG.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"RateATScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@rateAT.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"RateATScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@rateAT.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"RateCGScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@rateCG.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"RateCGScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@rateCG.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"RateGTScaler.s:", ids, "\" spec=\"ScaleOperator\" parameter=\"@rateGT.s:", ids, "\" scaleFactor=\"0.5\" weight=\"0.1\"/>"))
+    text <- c(text, paste0("    <operator id=\"RateGTScaler.s:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@rateGT.s:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"0.1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" delta=\"0.01\" weight=\"0.1\">"))
-    text <- c(text, paste0("        <parameter idref=\"freqParameter.s:", ids, "\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"FrequenciesExchanger.s:", ids, "\" spec=\"DeltaExchangeOperator\" ",
+      "delta=\"0.01\" weight=\"0.1\">"))
+    text <- c(text, paste0("        <parameter ",
+      "idref=\"freqParameter.s:", ids, "\"/>"))
     text <- c(text, paste0("    </operator>"))
   }
 
@@ -124,23 +155,36 @@ create_beast2_input_operators <- function(
       "\" scaleFactor=\"0.75\" weight=\"3.0\"/>"))
   } else if (is_cbs_tree_prior(tree_priors)) {
     text <- c(text, "")
-    text <- c(text, paste0("    <operator id=\"popSizesScaler.t:", ids, "\" spec=\"ScaleOperator\" parameter=\"@bPopSizes.t:", ids, "\" scaleFactor=\"0.75\" weight=\"15.0\"/>"))
+    text <- c(text, paste0("    <operator id=\"popSizesScaler.t:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@bPopSizes.t:", ids, "\" ",
+      "scaleFactor=\"0.75\" weight=\"15.0\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"groupSizesDelta.t:", ids, "\" spec=\"DeltaExchangeOperator\" integer=\"true\" weight=\"6.0\">"))
-    text <- c(text, paste0("        <intparameter idref=\"bGroupSizes.t:", ids, "\"/>"))
+    text <- c(text, paste0("    <operator id=\"groupSizesDelta.t:", ids, "\" ",
+      "spec=\"DeltaExchangeOperator\" integer=\"true\" weight=\"6.0\">"))
+    text <- c(text, paste0("        <intparameter ",
+      "idref=\"bGroupSizes.t:", ids, "\"/>"))
     text <- c(text, paste0("    </operator>"))
   }
 
   # Clock model
   if (is_relaxed_log_normal_clock_model(clock_models)) {
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"ucldStdevScaler.c:", ids, "\" spec=\"ScaleOperator\" parameter=\"@ucldStdev.c:", ids, "\" scaleFactor=\"0.5\" weight=\"3.0\"/>"))
+    text <- c(text, paste0("    <operator id=\"ucldStdevScaler.c:", ids, "\" ",
+      "spec=\"ScaleOperator\" parameter=\"@ucldStdev.c:", ids, "\" ",
+      "scaleFactor=\"0.5\" weight=\"3.0\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"CategoriesRandomWalk.c:", ids, "\" spec=\"IntRandomWalkOperator\" parameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\" windowSize=\"1\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"CategoriesRandomWalk.c:", ids, "\" spec=\"IntRandomWalkOperator\" ",
+      "parameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\" ",
+      "windowSize=\"1\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"CategoriesSwapOperator.c:", ids, "\" spec=\"SwapOperator\" intparameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"CategoriesSwapOperator.c:", ids, "\" spec=\"SwapOperator\" ",
+      "intparameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\"/>"))
     text <- c(text, paste0(""))
-    text <- c(text, paste0("    <operator id=\"CategoriesUniform.c:", ids, "\" spec=\"UniformOperator\" parameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\"/>"))
+    text <- c(text, paste0("    <operator ",
+      "id=\"CategoriesUniform.c:", ids, "\" spec=\"UniformOperator\" ",
+      "parameter=\"@rateCategories.c:", ids, "\" weight=\"10.0\"/>"))
   }
 
   text
