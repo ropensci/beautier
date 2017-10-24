4074d167c4921f8c554356a052fb61f0ca730937
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-24 09:55:52 +0200

    Fixed kappa conversion

--- tests/testthat/test-get_kappa.R
+++ tests/testthat/test-get_kappa.R
@@ -2,7 +2,8 @@ context("get_kappa")
 
 test_that("use", {
 
-  testthat::expect_equal(get_kappa(create_hky_site_model()), 2.0)
+
+  testthat::expect_equal(get_kappa(create_hky_site_model()), "2.0")
 
   testthat::expect_equal(get_kappa(create_hky_site_model(kappa = 1.2)), 1.2)
 })
