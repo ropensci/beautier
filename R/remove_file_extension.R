9d7ebb094b643aedbe8bc6958ee2a83ec5d45276
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-24 13:10:41 +0200

    Codecov

--- R/remove_file_extension.R
+++
@@ -1,7 +0,0 @@
-#' Remove a file extension
-#' @param filename A filename
-#' @return That filename without its extension
-#' @export
-remove_file_extension <- function(filename) {
-  return(tools::file_path_sans_ext(filename))
-}
