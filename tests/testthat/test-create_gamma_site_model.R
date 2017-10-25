397718a1d601831e88e1d3fe116f3112e00f98eb
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-25 13:56:18 +0200

    @lintr-bot

--- tests/testthat/test-create_gamma_site_model.R
+++ tests/testthat/test-create_gamma_site_model.R
@@ -10,7 +10,8 @@ test_that("Can specify HKY gamma category count", {
 
 test_that("Can specify HKY proportion invariant", {
 
-  gamma_site_model <- beastscriptr::create_gamma_site_model(prop_invariant = 0.2)
+  gamma_site_model <- beastscriptr::create_gamma_site_model(
+    prop_invariant = 0.2)
   testthat::expect_true(beastscriptr::is_gamma_site_model(gamma_site_model))
   testthat::expect_equal(get_prop_invariant(gamma_site_model), 0.2)
 
