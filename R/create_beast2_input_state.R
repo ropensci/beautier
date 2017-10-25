20f0d0e3dd35330908256893c42432033a7314c3
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-25 11:03:36 +0200

    Simplified create_beast2_input_state

--- R/create_beast2_input_state.R
+++ R/create_beast2_input_state.R
@@ -13,59 +13,15 @@ create_beast2_input_state <- function(
 ) {
   text <- NULL
   text <- c(text, "    <state id=\"state\" storeEvery=\"5000\">")
-  text <- c(text, create_beast2_input_state_tree(ids = ids, initial_phylogeny = initial_phylogeny))
+  text <- c(text, create_beast2_input_state_tree(
+    ids = ids, initial_phylogeny = initial_phylogeny))
+  text <- c(text, create_beast2_input_state_tree_priors(
+    ids = ids, tree_priors = tree_priors))
+  text <- c(text, create_beast2_input_state_site_models(
+    ids = ids, site_models = site_models))
+  text <- c(text, create_beast2_input_state_gamma_site_models(
+    ids = ids, site_models = site_models))
 
-  # Tree priors
-  if (is_yule_tree_prior(tree_priors)) {
-    text <- c(text, paste0("        <parameter id=\"birthRate.t:", ids, "\" ",
-      "name=\"stateNode\">1.0</parameter>"))
-  } else if (is_bd_tree_prior(tree_priors)) {
-    text <- c(text, paste0("        <parameter id=\"BDBirthRate.t:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\" upper=\"10000.0\">1.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"BDDeathRate.t:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.5</parameter>"))
-  } else if (is_ccp_tree_prior(tree_priors)) {
-    text <- c(text, paste0("        <parameter id=\"popSize.t:", ids, "\" ",
-      "name=\"stateNode\">0.3</parameter>"))
-  } else if (is_cbs_tree_prior(tree_priors)) {
-    text <- c(text, paste0("        <parameter id=\"bPopSizes.t:", ids, "\" ",
-      "dimension=\"5\" lower=\"0.0\" name=\"stateNode\" ",
-      "upper=\"380000.0\">380.0</parameter>"))
-    text <- c(text, paste0("        <stateNode id=\"bGroupSizes.t:", ids, "\" ",
-      "spec=\"parameter.IntegerParameter\" dimension=\"5\">1</stateNode>"))
-  }
-
-  # Site models, specific parameters
-  if (is_hky_site_model(site_models)) {
-    text <- c(text, paste0("        <parameter id=\"kappa.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">",
-      beastscriptr::get_kappa(site_models), "</parameter>"))
-  } else if (is_tn93_site_model(site_models)) {
-    text <- c(text, paste0("        <parameter id=\"kappa1.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">2.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"kappa2.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">2.0</parameter>"))
-  } else if (is_gtr_site_model(site_models)) {
-    text <- c(text, paste0("        <parameter id=\"rateAC.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"rateAG.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"rateAT.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"rateCG.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
-    text <- c(text, paste0("        <parameter id=\"rateGT.s:", ids, "\" ",
-      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
-  }
-
-  # Gamma site models
-  if (is_hky_site_model(site_models)
-    || is_tn93_site_model(site_models)
-    || is_gtr_site_model(site_models)) {
-    text <- c(text, paste0("        <parameter ",
-      "id=\"freqParameter.s:", ids, "\" dimension=\"4\" lower=\"0.0\" ",
-      "name=\"stateNode\" upper=\"1.0\">0.25</parameter>"))
-  }
 
   # Clock models
   if (is_rln_clock_model(clock_models)) {
@@ -108,6 +64,106 @@ create_beast2_input_state_tree <- function( # nolint long function name is fine,
         "adjustTipHeights=\"false\" taxa=\"@", ids, "\" ",
         "newick=\"", ape::write.tree(initial_phylogeny), "\">"))
     text <- c(text, paste0("    </stateNode>"))
+  }
+  text
+}
+
+
+#' Creates the tree priors part of the state section of a BEAST2
+#' XML parameter file
+#' @param ids the IDs of the alignments (can be extracted from
+#'   their FASTA filesnames using \code{\link{get_file_base_sans_ext}})
+#' @inheritParams create_beast2_input
+#' @note this function is not intended for regular use, thus its
+#'   long name length is accepted
+#' @author Richel J.C. Bilderbeek
+#' @export
+create_beast2_input_state_tree_priors <- function( # nolint long function name is fine, as (1) it follows a pattern (2) this function is not intended to be used regularily
+  ids,
+  tree_priors
+) {
+  text <- NULL
+  if (is_yule_tree_prior(tree_priors)) {
+    text <- c(text, paste0("        <parameter id=\"birthRate.t:", ids, "\" ",
+      "name=\"stateNode\">1.0</parameter>"))
+  } else if (is_bd_tree_prior(tree_priors)) {
+    text <- c(text, paste0("        <parameter id=\"BDBirthRate.t:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\" upper=\"10000.0\">1.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"BDDeathRate.t:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.5</parameter>"))
+  } else if (is_ccp_tree_prior(tree_priors)) {
+    text <- c(text, paste0("        <parameter id=\"popSize.t:", ids, "\" ",
+      "name=\"stateNode\">0.3</parameter>"))
+  } else if (is_cbs_tree_prior(tree_priors)) {
+    text <- c(text, paste0("        <parameter id=\"bPopSizes.t:", ids, "\" ",
+      "dimension=\"5\" lower=\"0.0\" name=\"stateNode\" ",
+      "upper=\"380000.0\">380.0</parameter>"))
+    text <- c(text, paste0("        <stateNode id=\"bGroupSizes.t:", ids, "\" ",
+      "spec=\"parameter.IntegerParameter\" dimension=\"5\">1</stateNode>"))
+  }
+  text
+}
+
+#' Creates the site_models part of the state section of a BEAST2
+#' XML parameter file
+#' @param ids the IDs of the alignments (can be extracted from
+#'   their FASTA filesnames using \code{\link{get_file_base_sans_ext}})
+#' @inheritParams create_beast2_input
+#' @note this function is not intended for regular use, thus its
+#'   long name length is accepted
+#' @author Richel J.C. Bilderbeek
+#' @export
+create_beast2_input_state_site_models <- function( # nolint long function name is fine, as (1) it follows a pattern (2) this function is not intended to be used regularily
+  ids,
+  site_models
+) {
+  text <- NULL
+  if (is_hky_site_model(site_models)) {
+    text <- c(text, paste0("        <parameter id=\"kappa.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">",
+      beastscriptr::get_kappa(site_models), "</parameter>"))
+  } else if (is_tn93_site_model(site_models)) {
+    text <- c(text, paste0("        <parameter id=\"kappa1.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">2.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"kappa2.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">2.0</parameter>"))
+  } else if (is_gtr_site_model(site_models)) {
+    text <- c(text, paste0("        <parameter id=\"rateAC.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"rateAG.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"rateAT.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"rateCG.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
+    text <- c(text, paste0("        <parameter id=\"rateGT.s:", ids, "\" ",
+      "lower=\"0.0\" name=\"stateNode\">1.0</parameter>"))
+  }
+  text
+}
+
+
+#' Creates the gamma_site_models part of the state section of a BEAST2
+#' XML parameter file
+#' @param ids the IDs of the alignments (can be extracted from
+#'   their FASTA filesnames using \code{\link{get_file_base_sans_ext}})
+#' @inheritParams create_beast2_input
+#' @note this function is not intended for regular use, thus its
+#'   long name length is accepted
+#' @author Richel J.C. Bilderbeek
+#' @export
+create_beast2_input_state_gamma_site_models <- function( # nolint long function name is fine, as (1) it follows a pattern (2) this function is not intended to be used regularily
+  ids,
+  site_models
+) {
+  text <- NULL
+  # Gamma site models
+  if (is_hky_site_model(site_models)
+    || is_tn93_site_model(site_models)
+    || is_gtr_site_model(site_models)) {
+    text <- c(text, paste0("        <parameter ",
+      "id=\"freqParameter.s:", ids, "\" dimension=\"4\" lower=\"0.0\" ",
+      "name=\"stateNode\" upper=\"1.0\">0.25</parameter>"))
   }
   text
 }
