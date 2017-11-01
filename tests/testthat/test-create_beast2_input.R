b541eca1c9cfce3fcc3723c60f11155e962b280c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-01 12:54:21 +0100

    gtr_2_4.xml is reproduced

--- tests/testthat/test-create_beast2_input.R
+++ tests/testthat/test-create_beast2_input.R
@@ -103,17 +103,6 @@ test_that("Check that gtr_2_4.xml is reproduced", {
   expected_lines <- readLines(system.file("extdata",
     "gtr_2_4.xml", package = "beautier"))
 
-  if (1 == 2) { # nolint keep this to help fixing future tests
-    write.csv(created_lines, "~/created.csv")
-    write.csv(expected_lines, "~/expected.csv")
-    for (i in 1:min(length(expected_lines), length(created_lines))) {
-      testthat::expect_equal(
-        expected_lines[i], created_lines[i]
-      )
-      print(paste0(i, " / ", length(expected_lines)))
-    }
-  }
-
   testthat::expect_identical(created_lines, expected_lines)
 })
 
