247fde5986fc56563795db72f059f72b97d81e4e
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-07 11:56:35 +0100

    Use list() instead of as_list()

--- tests/testthat/test-as_list.R
+++
@@ -1,9 +0,0 @@
-context("as_list")
-
-test_that("example", {
-
-  v <- as_list(create_hky_site_model(), create_jc69_site_model())
-  testit::assert(is_hky_site_model(v[[1]]))
-  testit::assert(is_jc69_site_model(v[[2]]))
-
-})
