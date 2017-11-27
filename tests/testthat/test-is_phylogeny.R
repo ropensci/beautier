024e6694f30c8dc10ad34895bb36f32baac378bc
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-27 13:24:56 +0100

    @lintr-bot

--- tests/testthat/test-is_phylogeny.R
+++ tests/testthat/test-is_phylogeny.R
@@ -5,7 +5,7 @@ test_that("is_phylogeny: use", {
   testthat::expect_true(is_phylogeny(ape::rcoal(n = 5)))
   testthat::expect_false(is_phylogeny(ape::rmtree(N = 2, n = 10)))
   testthat::expect_false(is_phylogeny(42))
-  testthat::expect_false(is_phylogeny(c(1,2,3)))
+  testthat::expect_false(is_phylogeny(c(1, 2, 3)))
   testthat::expect_false(is_phylogeny(3.14))
   testthat::expect_false(is_phylogeny("Hello"))
 
