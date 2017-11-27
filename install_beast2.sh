d9a8be183c926bb2780b19e20ff210acabe486fc
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-27 10:05:54 +0100

    Use BEAST (and BEAUti) v2.4.7

--- install_beast2.sh
+++ install_beast2.sh
@@ -2,5 +2,7 @@
 cd ~
 mkdir Programs
 cd Programs
-wget https://github.com/CompEvol/beast2/releases/download/v2.4.0/BEAST.v2.4.0.Linux.tgz
-tar zxvf BEAST.v2.4.0.Linux.tgz
+wget https://github.com/CompEvol/beast2/releases/download/v2.4.0/BEAST.v2.4.7.Linux.tgz
+# wget https://github.com/CompEvol/beast2/releases/download/v2.4.0/BEAST.v2.4.0.Linux.tgz
+tar zxvf BEAST.v2.4.7.Linux.tgz
+# tar zxvf BEAST.v2.4.0.Linux.tgz
