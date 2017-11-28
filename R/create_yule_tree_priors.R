9aa67aba7a63b26e19e221414e1b977b9a44a5d4
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-28 09:19:40 +0100

    Doc

--- R/create_yule_tree_priors.R
+++ R/create_yule_tree_priors.R
@@ -2,11 +2,11 @@
 #' @param n the number of Yule tree_prior objects
 #' @return a list of Yule tree_prior objects
 #' @examples
-#'   m <- create_yule_tree_priors(1)
+#'   m <- create_yule_tree_priors(ids = "some_id")
 #'   testthat::expect_equal(length(m), 1)
 #'   testthat::expect_true(is_yule_tree_prior(m[[1]]))
 #'
-#'   m <- create_yule_tree_priors(2)
+#'   m <- create_yule_tree_priors(ids = c("a", "b"))
 #'   testthat::expect_equal(length(m), 2)
 #'   testthat::expect_true(is_yule_tree_prior(m[[1]]))
 #'   testthat::expect_true(is_yule_tree_prior(m[[2]]))
