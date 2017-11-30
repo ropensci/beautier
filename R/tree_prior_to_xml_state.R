324c029a3f9f32d24f88cdfdc0f79e7955266dab
richelbilderbeek <richel@richelbilderbeek.nl>
2017-11-30 10:58:14 +0100

    Doc

--- R/tree_prior_to_xml_state.R
+++ R/tree_prior_to_xml_state.R
@@ -1,4 +1,9 @@
-#' @export
+#' Creates the XML of a tree prior,
+#'   as used in the \code{state} section
+#' @param tree_prior a tree prior,
+#'   as returned by \code{\link{create_tree_prior}})
+#' @return the tree prior as XML text
+#' @author Richel J.C. Bilderbeek
 tree_prior_to_xml_state <- function(
   tree_prior
 ) {
