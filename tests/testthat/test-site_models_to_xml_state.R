1a0e8bba69d1052bb197337145d7714fa559d301
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-06 18:11:17 +0100

    All state sections OK, except aco_nd2_nd3_nd4_complex_2_4.xml

--- tests/testthat/test-site_models_to_xml_state.R
+++ tests/testthat/test-site_models_to_xml_state.R
@@ -242,23 +242,34 @@ test_that("JC69 JC69 JC69 JC69 shared site model", {
 test_that("HKY 2 HKY 2 HKY 2 HKY 2 GCC diff shapes diff prop_inv", {
 
   expected <- c(
-
+    "<parameter id=\"gammaShape.s:anthus_aco\" name=\"stateNode\">1.1</parameter>",
+    "<parameter id=\"gammaShape.s:anthus_nd2\" name=\"stateNode\">1.2</parameter>",
+    "<parameter id=\"gammaShape.s:anthus_nd3\" name=\"stateNode\">1.3</parameter>",
+    "<parameter id=\"gammaShape.s:anthus_nd4\" name=\"stateNode\">1.4</parameter>",
+    "<parameter id=\"kappa.s:anthus_aco\" lower=\"0.0\" name=\"stateNode\">2.0</parameter>",
+    "<parameter id=\"kappa.s:anthus_nd2\" lower=\"0.0\" name=\"stateNode\">2.0</parameter>",
+    "<parameter id=\"kappa.s:anthus_nd3\" lower=\"0.0\" name=\"stateNode\">2.0</parameter>",
+    "<parameter id=\"kappa.s:anthus_nd4\" lower=\"0.0\" name=\"stateNode\">2.0</parameter>",
+    "<parameter id=\"freqParameter.s:anthus_nd2\" dimension=\"4\" lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.25</parameter>",
+    "<parameter id=\"freqParameter.s:anthus_nd4\" dimension=\"4\" lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.25</parameter>",
+    "<parameter id=\"freqParameter.s:anthus_nd3\" dimension=\"4\" lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.25</parameter>",
+    "<parameter id=\"freqParameter.s:anthus_aco\" dimension=\"4\" lower=\"0.0\" name=\"stateNode\" upper=\"1.0\">0.25</parameter>"
   )
   created <- beautier:::site_models_to_xml_state(
     site_models = list(
-      create_jc69_site_model(id = "anthus_aco",
+      create_hky_site_model(id = "anthus_aco",
         gamma_site_model = create_gamma_site_model(
           gamma_cat_count = 2, gamma_shape = "1.1", prop_invariant = 0.1)
       ),
-      create_jc69_site_model(id = "anthus_nd2",
+      create_hky_site_model(id = "anthus_nd2",
         gamma_site_model = create_gamma_site_model(
           gamma_cat_count = 2, gamma_shape = "1.2", prop_invariant = 0.2)
       ),
-      create_jc69_site_model(id = "anthus_nd3",
+      create_hky_site_model(id = "anthus_nd3",
         gamma_site_model = create_gamma_site_model(
           gamma_cat_count = 2, gamma_shape = "1.3", prop_invariant = 0.3)
       ),
-      create_jc69_site_model(id = "anthus_nd4",
+      create_hky_site_model(id = "anthus_nd4",
         gamma_site_model = create_gamma_site_model(
           gamma_cat_count = 2, gamma_shape = "1.4", prop_invariant = 0.4)
       )
