89d43492561472698be699ce6938bd4f29f0d70b
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-08 10:19:51 +0100

    'get_file_base_sans_ext' is not exported

--- R/get_id.R
+++ R/get_id.R
@@ -14,7 +14,7 @@ get_id <- function(
   fasta_filename,
   capitalize_first_char_id = FALSE
 ) {
-  id <- beautier::get_file_base_sans_ext(fasta_filename)
+  id <- get_file_base_sans_ext(fasta_filename)
   if (capitalize_first_char_id == TRUE) {
     id <- paste0(toupper(substr(id, 1, 1)), substring(id, 2))
   }
