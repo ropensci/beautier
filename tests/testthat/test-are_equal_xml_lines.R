08081ff278d79c08cd132972981934fabb8aa269
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-12 11:10:39 +0100

    Codecov

--- tests/testthat/test-are_equal_xml_lines.R
+++ tests/testthat/test-are_equal_xml_lines.R
@@ -29,33 +29,43 @@ test_that("use", {
 
 })
 
-test_that("abuse", {
+test_that("abuse: section must be a word", {
 
-  lines_1 <- c(
-    "<a>",
-    "  same",
-    "</a>",
-    "<b>",
-    "  different from lines_2",
-    "</b>"
-  )
-
-  lines_2 <- c(
-    "<a>",
-    "  same",
-    "</a>",
-    "<b>",
-    "  different from lines_1",
-    "</b>"
-  )
-
-  testthat::expect_error(
-    are_equal_xml_lines(lines_1, lines_2, section = "nonsense"),
-    "Opening tag for 'section' could not be found in 'lines_1'"
-  )
   testthat::expect_error(
     are_equal_xml_lines(lines_1, lines_2, section = NA),
     "'section' must be a word"
   )
 
+})
+
+test_that("abuse: opening tag of lines 1 not found", {
+
+  lines_1 <- c(
+    "<a>",
+    "  same",
+    "</a>"
+  )
+
+  lines_2 <- lines_1
+
+  testthat::expect_error(
+    are_equal_xml_lines(lines_1, lines_2, section = "nonsense"),
+    "Opening tag for 'section' could not be found in 'lines_1'"
+  )
+})
+
+test_that("abuse: closing tag of lines 1 not found", {
+
+  lines_1 <- c(
+    "<a>",
+    "  same",
+    "<oops_a>"
+  )
+
+  lines_2 <- lines_1
+
+  testthat::expect_error(
+    are_equal_xml_lines(lines_1, lines_2, section = "a"),
+    "Closing tag for 'section' could not be found in 'lines_1'"
+  )
 })
