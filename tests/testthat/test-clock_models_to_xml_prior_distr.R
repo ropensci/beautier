b5c9e7e598ebc4ff1bae68719e6cf70e1907fede
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-12 12:11:20 +0100

    Remove 'collect_ids'

--- tests/testthat/test-clock_models_to_xml_prior_distr.R
+++ tests/testthat/test-clock_models_to_xml_prior_distr.R
@@ -532,3 +532,48 @@ test_that("RLN", {
   )
   testthat::expect_true(beautier:::are_equivalent_xml_lines(created, expected))
 })
+
+
+
+
+
+test_that("strict shared", {
+
+  expected <- c(
+    "<distribution id=\"prior\" spec=\"util.CompoundDistribution\">", # nolint XML
+    "    <distribution id=\"YuleModel.t:anthus_aco\" spec=\"beast.evolution.speciation.YuleModel\" birthDiffRate=\"@birthRate.t:anthus_aco\" tree=\"@Tree.t:anthus_aco\"/>", # nolint XML
+    "    <distribution id=\"YuleModel.t:anthus_nd2\" spec=\"beast.evolution.speciation.YuleModel\" birthDiffRate=\"@birthRate.t:anthus_nd2\" tree=\"@Tree.t:anthus_nd2\"/>", # nolint XML
+    "    <prior id=\"YuleBirthRatePrior.t:anthus_aco\" name=\"distribution\" x=\"@birthRate.t:anthus_aco\">", # nolint XML
+    "        <Uniform id=\"Uniform.1\" name=\"distr\" upper=\"Infinity\"/>", # nolint XML
+    "    </prior>", # nolint XML
+    "    <prior id=\"YuleBirthRatePrior.t:anthus_nd2\" name=\"distribution\" x=\"@birthRate.t:anthus_nd2\">", # nolint XML
+    "        <Uniform id=\"Uniform.4\" name=\"distr\" upper=\"Infinity\"/>", # nolint XML
+    "    </prior>", # nolint XML
+    "</distribution>" # nolint XML
+  )
+  created <- beautier:::create_beast2_input_distr_prior(
+    site_models = list(
+      create_jc69_site_model(id = "anthus_aco"),
+      create_jc69_site_model(id = "anthus_nd2")
+    ),
+    clock_models = list(
+      create_strict_clock_model(
+        id = "anthus_aco",
+        clock_rate_distr = create_uniform_distr(id = 3)
+      ),
+      create_strict_clock_model(
+        id = "anthus_aco",
+        clock_rate_distr = create_uniform_distr(id = 4)
+      )
+    ),
+    tree_priors = list(
+      create_yule_tree_prior(
+        id = "anthus_aco", birth_rate_distr = create_uniform_distr(id = 1)),
+      create_yule_tree_prior(
+        id = "anthus_nd2", birth_rate_distr = create_uniform_distr(id = 4))
+    )
+  )
+  skip("WIP: distr prior shared strict")
+  testthat::expect_true(beautier:::are_equivalent_xml_lines(created, expected))
+  beautier:::compare_lines(created, expected)
+})
