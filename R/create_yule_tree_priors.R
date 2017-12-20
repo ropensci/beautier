478e8823a7fe6ee578b0a0fc7f934b0ba628c63d
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-20 16:46:42 +0100

    Doc

--- R/create_yule_tree_priors.R
+++ R/create_yule_tree_priors.R
@@ -2,14 +2,16 @@
 #' @param ids the alignment IDs
 #' @return a list of Yule tree_prior objects
 #' @examples
-#'   m <- create_yule_tree_priors(ids = "some_id")
-#'   testthat::expect_equal(length(m), 1)
-#'   testthat::expect_true(is_yule_tree_prior(m[[1]]))
+#'   fasta_filenames <- get_paths(c("anthus_aco.fas", "anthus_nd2.fas"))
 #'
-#'   m <- create_yule_tree_priors(ids = c("a", "b"))
-#'   testthat::expect_equal(length(m), 2)
-#'   testthat::expect_true(is_yule_tree_prior(m[[1]]))
-#'   testthat::expect_true(is_yule_tree_prior(m[[2]]))
+#'   tree_priors <- create_yule_tree_priors(ids = get_ids(fasta_filenames))
+#'
+#'   create_beast2_input_file(
+#'     fasta_filenames,
+#'     "create_yule_tree_priors.xml",
+#'     tree_priors = tree_priors
+#'   )
+#'   testit::assert(file.exists("create_yule_tree_priors.xml"))
 #' @export
 create_yule_tree_priors <- function(ids) {
   n <- length(ids)
