883ebb357d18aa29e22ed80ae002ab11e20ad4a8
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-22 12:38:48 +0100

    Do not keep test.txt

--- R/fastas_to_phylos.R
+++ R/fastas_to_phylos.R
@@ -24,7 +24,7 @@
 #' @export
 fastas_to_phylos <- function(fasta_filenames, crown_age) {
 
-  if (!beautier:::files_exist(fasta_filenames)) {
+  if (!files_exist(fasta_filenames)) {
     stop("'fasta_filenames' must be the names of existing files")
   }
   if (crown_age <= 0.0) {
