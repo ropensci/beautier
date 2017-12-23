e50d5c29450a304d143f468b21d3a335e123cd8c
richelbilderbeek <richel@richelbilderbeek.nl>
2017-12-23 18:11:59 +0100

    All tests pass locally, also Travis tests

--- tests/testthat/test-create_beast2_input_file_1_12.R
+++ tests/testthat/test-create_beast2_input_file_1_12.R
@@ -1,9 +1,9 @@
-context("create_beast2_input_file")
+context("create_beast2_input_file_1_12")
 
 test_that("checks input", {
 
-  # Don't: input is checked by 'create_beast2_input'
-  # See 'create_beast2_input' tests
+  # Don't: input is checked by 'create_beast2_input_1_12'
+  # See 'create_beast2_input_1_12' tests
 })
 
 test_that("Can specify fixed crown age", {
@@ -13,10 +13,12 @@ test_that("Can specify fixed crown age", {
   input_fasta_filename <- beautier::get_fasta_filename()
   output_xml_filename_fixed <- tempfile()
 
-  beautier::create_beast2_input_file(
+  beautier::create_beast2_input_file_1_12(
     input_fasta_filenames = input_fasta_filename,
     output_xml_filename = output_xml_filename_fixed,
-    posterior_crown_age = 15
+    fixed_crown_ages = TRUE,
+    initial_phylogenies = beautier::fasta_to_phylo(
+      input_fasta_filename, crown_age = 15)
   )
   testthat::expect_true(
     beastier:::is_beast2_input_file(output_xml_filename_fixed)
@@ -30,10 +32,12 @@ test_that("Can specify fixed crown ages", {
   input_fasta_filenames <- get_paths(c("anthus_aco.fas", "anthus_nd2.fas"))
   output_xml_filename_fixed <- tempfile()
 
-  beautier::create_beast2_input_file(
+  beautier::create_beast2_input_file_1_12(
     input_fasta_filenames = input_fasta_filenames,
     output_xml_filename = output_xml_filename_fixed,
-    posterior_crown_age = 15
+    fixed_crown_ages = c(TRUE, TRUE),
+    initial_phylogenies = beautier::fastas_to_phylos(
+      input_fasta_filenames, crown_age = 15)
   )
   testthat::expect_true(
     beastier:::is_beast2_input_file(output_xml_filename_fixed)
