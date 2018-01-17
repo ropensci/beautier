053674542485c0d1dafb39b074fa0672bec3175b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-17 12:29:27 +0100

    Doc

--- R/is_mcmc.R
+++ R/is_mcmc.R
@@ -1,6 +1,6 @@
 #' Determine if the object is a valid mcmc object
 #' @param x an object, to be determined if it is a valid mcmc object
-#' @seealso Use \link{\code{create_mcmc}}
+#' @seealso Use \code{\link{create_mcmc}}
 #' @return TRUE if x is a valid mcmc object, FALSE otherwise
 #' @author Richel J.C. Bilderbeek
 is_mcmc <- function(
