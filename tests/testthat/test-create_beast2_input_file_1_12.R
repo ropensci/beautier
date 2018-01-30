67469c758ca83010f67241dc0ab8310be56b0e9a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-01-30 11:59:43 +0100

    Add dataset 'anthus_X_X.xml' dataset, in which X is a crown age

--- tests/testthat/test-create_beast2_input_file_1_12.R
+++ tests/testthat/test-create_beast2_input_file_1_12.R
@@ -15,3 +15,61 @@ test_that("use", {
   testthat::expect_true(file.exists(output_filename))
 
 })
+
+test_that("create data set of two alignments, with/out fixed crown ages", {
+
+  output_filename <- tempfile()
+  testit::assert(!file.exists(output_filename))
+
+  fasta_filename_1 <- get_path("anthus_aco.fas")
+  fasta_filename_2 <- get_path("anthus_nd2.fas")
+  fasta_filenames <- c(fasta_filename_1, fasta_filename_2)
+
+  phylo_1_15 <- fasta_to_phylo(fasta_filename_1, crown_age = 15)
+  phylo_2_15 <- fasta_to_phylo(fasta_filename_2, crown_age = 15)
+  phylo_2_26 <- fasta_to_phylo(fasta_filename_2, crown_age = 26)
+
+  testthat::expect_silent(
+    create_beast2_input_file_1_12(
+      fasta_filenames,
+      "anthus_na_na.xml"
+    )
+  )
+
+  testthat::expect_silent(
+    create_beast2_input_file_1_12(
+      fasta_filenames,
+      "anthus_15_15.xml",
+      fixed_crown_ages = c(TRUE, TRUE),
+      initial_phylogenies = list(phylo_1_15, phylo_2_15)
+    )
+  )
+
+  testthat::expect_silent(
+    create_beast2_input_file_1_12(
+      fasta_filenames,
+      "anthus_na_15.xml",
+      fixed_crown_ages = c(FALSE, TRUE),
+      initial_phylogenies = list(NA, phylo_2_15)
+    )
+  )
+
+    testthat::expect_silent(
+    create_beast2_input_file_1_12(
+      fasta_filenames,
+      "anthus_15_na.xml",
+      fixed_crown_ages = c(TRUE, FALSE),
+      initial_phylogenies = list(phylo_1_15, NA)
+    )
+  )
+
+  testthat::expect_silent(
+    create_beast2_input_file_1_12(
+      fasta_filenames,
+      "anthus_15_26.xml",
+      fixed_crown_ages = c(TRUE, TRUE),
+      initial_phylogenies = list(phylo_1_15, phylo_2_26)
+    )
+  )
+
+})
