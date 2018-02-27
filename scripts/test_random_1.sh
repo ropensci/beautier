69aa17e702be1c4d0e4ba4611034a5ada6b7d4d5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-27 14:50:22 +0100

    Use job number in log file name [skip ci]

--- scripts/test_random_1.sh
+++ scripts/test_random_1.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --ntasks=1
 #SBATCH --mem=1G
-#SBATCH --job-name=test_random_1_%j
-#SBATCH --output=test_random_1.log
+#SBATCH --job-name=test_random_1
+#SBATCH --output=test_random_1_%j.log
 module load R/3.3.1-foss-2016a
 time Rscript -e 'source("test_random_1.R")'
