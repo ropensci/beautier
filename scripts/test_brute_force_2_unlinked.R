ea382dc873dea4c8ec4c32d994dd3d930e336987
richelbilderbeek <richel@richelbilderbeek.nl>
2018-02-27 13:19:54 +0100

    Fix script

--- scripts/test_brute_force_2_unlinked.R
+++ scripts/test_brute_force_2_unlinked.R
@@ -12,19 +12,19 @@ for (site_model_1 in beautier:::create_site_models()) {
       for (clock_model_2 in beautier:::create_clock_models()) {
         for (tree_prior in beautier:::create_tree_priors()) {
           cat(".")
-          output_xml_filename <- "~/invalid.xml"
+          output_filename <- "~/invalid.xml"
           create_beast2_input_file(
             input_filenames = input_filenames,
             site_models = list(site_model_1, site_model_2),
             clock_models = list(clock_model_1, clock_model_2),
             tree_priors = list(tree_prior, tree_prior),
-            output_xml_filename = output_xml_filename
+            output_filename = output_filename
           )
-          is_ok <- beastier::is_beast2_input_file(output_xml_filename)
+          is_ok <- beastier::is_beast2_input_file(output_filename)
           if (!is_ok) {
             print(paste(site_model_1$name, site_model_2$name,
               clock_model_1$name, clock_model_2$name, tree_prior$name))
-            beautier::is_beast2_input_file(output_xml_filename,
+            beautier::is_beast2_input_file(output_filename,
               verbose = TRUE)
             n_fail <- n_fail + 1
           }
@@ -42,24 +42,24 @@ for (site_model_1 in beautier:::create_site_models()) {
         for (tree_prior in beautier:::create_tree_priors()) {
           if (runif(n = 1) < 0.9) next
           cat(".")
-          output_xml_filename <- "~/invalid.xml"
+          output_filename <- "~/invalid.xml"
           create_beast2_input_file(
             input_filenames = input_filenames,
             site_models = list(site_model_1, site_model_2),
             clock_models = list(clock_model_1, clock_model_2),
             tree_priors = list(tree_prior, tree_prior),
-            output_xml_filename = output_xml_filename,
+            output_filename = output_filename,
             fixed_crown_ages = c(TRUE, TRUE),
             initial_phylogenies = fastas_to_phylos(
               fasta_filenames = input_filenames,
               crown_age = 15
             )
           )
-          is_ok <- beastier::is_beast2_input_file(output_xml_filename)
+          is_ok <- beastier::is_beast2_input_file(output_filename)
           if (!is_ok) {
             print(paste(site_model_1$name, site_model_2$name,
               clock_model_1$name, clock_model_2$name, tree_prior$name))
-            beautier::is_beast2_input_file(output_xml_filename,
+            beautier::is_beast2_input_file(output_filename,
               verbose = TRUE)
             n_fail <- n_fail + 1
           }
@@ -76,7 +76,7 @@ for (site_model_1 in beautier:::create_site_models()) {
       for (clock_model_2 in beautier:::create_clock_models()) {
         for (tree_prior in beautier:::create_tree_priors()) {
           if (runif(n = 1) < 0.9) next
-          output_xml_filename <- "~/invalid.xml"
+          output_filename <- "~/invalid.xml"
           initial_phylenies <- fastas_to_phylos(
               fasta_filenames = input_filenames[1],
               crown_age = 15
@@ -87,15 +87,15 @@ for (site_model_1 in beautier:::create_site_models()) {
             site_models = list(site_model_1, site_model_2),
             clock_models = list(clock_model_1, clock_model_2),
             tree_priors = list(tree_prior, tree_prior),
-            output_xml_filename = output_xml_filename,
+            output_filename = output_filename,
             fixed_crown_ages = c(TRUE, FALSE),
             initial_phylogenies = initial_phylenies
           )
-          is_ok <- beastier::is_beast2_input_file(output_xml_filename)
+          is_ok <- beastier::is_beast2_input_file(output_filename)
           if (!is_ok) {
             print(paste(site_model_1$name, site_model_2$name,
               clock_model_1$name, clock_model_2$name, tree_prior$name))
-            beautier::is_beast2_input_file(output_xml_filename,
+            beautier::is_beast2_input_file(output_filename,
               verbose = TRUE)
             n_fail <- n_fail + 1
           }
