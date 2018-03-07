f5ca54bbf7d3238ac3875a08ee687defe60d89d5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-07 22:37:47 +0100

    Brute-force tests on MRCA prior fail

--- scripts/test_brute_force_1.R
+++ scripts/test_brute_force_1.R
@@ -130,7 +130,7 @@ brute_force_1_combinations_fixed_crown_age <- function() {
 
   n_fail <- 0
 
-  input_fasta_filename <- beautier::get_path("anthus_aco.fas")
+  input_fasta_filename <- beautier::get_beautier_path("anthus_aco.fas")
 
   for (site_model in beautier:::create_site_models()) {
     for (clock_model in beautier:::create_clock_models()) {
@@ -163,24 +163,25 @@ brute_force_1_combinations_calibration <- function() {
 
   n_fail <- 0
 
-  input_fasta_filename <- beautier::get_path("anthus_aco.fas")
+  input_fasta_filename <- beautier::get_beautier_path("anthus_aco.fas")
 
   for (site_model in beautier:::create_site_models()) {
     for (clock_model in beautier:::create_clock_models()) {
       for (tree_prior in beautier:::create_tree_priors()) {
 
-        output_xml_filename <- tempfile()
+        output_xml_filename <- "/home/richel/bad.xml"
+        # output_xml_filename <- tempfile()
         create_beast2_input_file(
           input_filenames = input_fasta_filename,
           site_models = site_model,
           clock_models = clock_model,
           tree_priors = tree_prior,
           output_filename = output_xml_filename,
-          posterior_crown_age = 15,
+          # posterior_crown_age = 15,
           mrca_priors = create_mrca_prior(
+            alignment_id = get_alignment_id(input_fasta_filename),
             taxa_names = get_taxa_names(input_fasta_filename)
           )
-
         )
         is_ok <- beastier::is_beast2_input_file(output_xml_filename)
         testthat::expect_true(is_ok)
