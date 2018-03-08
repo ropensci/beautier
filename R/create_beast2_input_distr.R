6201fcee8f4b6d25c16d291d5db4b2c52d14bed9
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-08 12:24:28 +0100

    @lintr-bot

--- R/create_beast2_input_distr.R
+++ R/create_beast2_input_distr.R
@@ -83,7 +83,10 @@ create_beast2_input_distr_prior <- function( # nolint internal function
   text <- c(text, tree_priors_to_xml_prior_distr(tree_priors)) # nolint internal function
   text <- c(text, gamma_site_models_to_xml_prior_distr(site_models)) # nolint internal function
   text <- c(text, site_models_to_xml_prior_distr(site_models)) # nolint internal function
-  text <- c(text, mrca_priors_to_xml_prior_distr(mrca_priors, has_non_strict_clock_model = get_has_non_strict_clock_model(clock_models))) # nolint internal function
+  text <- c(text, mrca_priors_to_xml_prior_distr( # nolint internal function
+    mrca_priors,
+    has_non_strict_clock_model = get_has_non_strict_clock_model(clock_models))
+  )
   text <- c(text, clock_models_to_xml_prior_distr(clock_models)) # nolint internal function
 
   text <- indent(text, n_spaces = 4) # nolint internal function
