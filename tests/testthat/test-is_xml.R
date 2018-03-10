74ed4af55fdd95797fe5e4270e0147459b0490c3
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-10 11:06:33 +0100

    Expose flaw in 'is_xml'

--- tests/testthat/test-is_xml.R
+++ tests/testthat/test-is_xml.R
@@ -5,5 +5,6 @@ test_that("use", {
   testthat::expect_false(beautier:::is_xml(NA))
   testthat::expect_true(beautier:::is_xml("<text content=\"Hello\"/>"))
   testthat::expect_true(beautier:::is_xml(beautier:::parameter_to_xml(create_param_alpha(id = 1))))
-  testthat::expect_false(beautier:::is_xml("<text>Hello</invalid_close>"))
+  skip("WIP")
+  testthat::expect_false(beautier:::is_xml("<text id=1</invalid_close>"))
 })
