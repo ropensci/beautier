bdd555917685a6b8e00174c213970f609ae2913e
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-12 12:35:21 +0100

    Expose bug in create_mrca_prior

--- scripts/test_random_1.R
+++ scripts/test_random_1.R
@@ -3,15 +3,20 @@ library(beautier)
 create_random <- function(
   input_fasta_filename = beautier:::get_beautier_path("anthus_aco.fas")
 ) {
-  input_filename <- beautier:::get_beautier_path("anthus_aco.fas"),
+  input_filename <- beautier:::get_beautier_path("anthus_aco.fas")
   output_xml_filename <- tempfile()
+  site_model <- beautier:::create_rnd_site_model()
+  clock_model <- beautier:::create_rnd_clock_model()
+  tree_prior <- beautier:::create_rnd_tree_prior()
+  mrca_prior <- beautier:::create_rnd_mrca_prior(input_filename)
+
   create_beast2_input_file(
     input_filenames = input_filename,
     output_filename = output_xml_filename,
-    site_models = beautier:::create_rnd_site_model(),
-    clock_models = beautier:::create_rnd_clock_model(),
-    tree_priors = beautier:::create_rnd_tree_prior(),
-    mrca_priors = beautier:::create_rnd_mrca_prior(input_filename)
+    site_models = site_model,
+    clock_models = clock_model,
+    tree_priors = tree_prior,
+    mrca_priors = mrca_prior
   )
   is_ok <- beastier::is_beast2_input_file(output_xml_filename)
   if (!is_ok) {
@@ -24,12 +29,14 @@ create_random <- function(
     print(clock_model)
     print("tree prior:")
     print(tree_prior)
+    print("mrca prior:")
+    print(mrca_prior)
   }
   is_ok
 }
 
 seed <- as.integer((as.double(Sys.time())*1000+Sys.getpid()) %% 2^31)
-#seed <- 0
+seed <- 0
 set.seed(seed)
 print(paste("seed:", seed))
 
