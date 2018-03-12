d87d40a733d91ada2dc1a3dacc751ce680eb194b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-12 08:50:58 +0100

    Doc

--- R/is_one_na.R
+++ R/is_one_na.R
@@ -2,12 +2,12 @@
 #' @param x the object to be determined if it is one NA
 #' @return TRUE if x is one NA, FALSE otherwise
 #' @examples
-#'   testit::assert(is_one_na(NA))
-#'   testit::assert(!is_one_na(NULL))
-#'   testit::assert(!is_one_na(42))
-#'   testit::assert(!is_one_na("Hello"))
-#'   testit::assert(!is_one_na(3.14))
-#'   testit::assert(!is_one_na(c(NA, NA)))
+#'   testit::assert(beautier:::is_one_na(NA))
+#'   testit::assert(!beautier:::is_one_na(NULL))
+#'   testit::assert(!beautier:::is_one_na(42))
+#'   testit::assert(!beautier:::is_one_na("Hello"))
+#'   testit::assert(!beautier:::is_one_na(3.14))
+#'   testit::assert(!beautier:::is_one_na(c(NA, NA)))
 #' @author Richel J.C. Bilderbeek
 is_one_na <- function(x) {
   length(x) == 1 && is.na(x)
