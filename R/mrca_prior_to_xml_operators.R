d5b7eb3e3f7756e62603199fb434d26d1325b477
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-15 13:18:37 +0100

    Progress #30

--- R/mrca_prior_to_xml_operators.R
+++ R/mrca_prior_to_xml_operators.R
@@ -1,16 +1,19 @@
 #' Creates the XML of an MRCA prior,
 #'   as used in the \code{operators} section
 #' @inheritParams default_params_doc
+#' @param is_first is this the first MRCA prior?
 #' @return the mrca prior as XML text
 #' @author Richel J.C. Bilderbeek
 mrca_prior_to_xml_operators <- function(
   mrca_prior,
   fixed_crown_age = FALSE,
-  has_non_strict_clock_model = FALSE
+  has_non_strict_clock_model = FALSE,
+  is_first = TRUE
 ) {
   testit::assert(is_mrca_prior(mrca_prior))
   if (length(mrca_prior) == 1 && is.na(mrca_prior)) return(NULL)
   if (!mrca_prior$is_monophyletic) return(NULL)
+  if (!is_first) return(NULL)
   if (has_non_strict_clock_model) return(NULL)
 
   id <- mrca_prior$alignment_id
