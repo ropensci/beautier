143dbb2d8b6c721a60a7f691c96c515717c101a2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-01 15:19:25 +0200

    No links to other packages

--- R/beautier.R
+++ R/beautier.R
@@ -9,17 +9,17 @@
 #' These are packages associated with 'beautier':
 #' \itemize{
 #'   \item{
-#'     The package \link[beastier]{beastier} can run
+#'     The package 'beastier' can run
 #'     BEAST2 from R
 #'   }
 #'   \item{
-#'     The package \link[tracerer]{tracerer} can parse
+#'     The package 'tracerer' can parse
 #'     BEAST2 output files from R
 #'   }
 #'   \item{
-#'     The package \link[babette]{babette} combines the
-#'     functionality of \link[beautier]{beautier},
-#'     \link[beastier]{beastier} and \link[tracerer]{tracerer}
+#'     The package 'babette' combines the
+#'     functionality of 'beautier',
+#'     'beastier' and 'tracerer'
 #'     into a single workflow
 #'   }
 #' }
