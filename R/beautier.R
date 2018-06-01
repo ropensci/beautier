00f72f8228a077a4180c2d1a728fecee86d738fb
richelbilderbeek <richel@richelbilderbeek.nl>
2018-06-01 12:15:04 +0200

    Add package documentation, thanks @bjoelle

---
+++ R/beautier.R
@@ -0,0 +1,10 @@
+#' beautier: A package to create a BEAST2 input file,
+#'
+#' 'beautier' allows to create a BEAST2 input file, using
+#' an R interface. 'beautier' closely follows the interface
+#' of BEAUti 2, a GUI tool bundled with BEAST2, including
+#' its default settings.
+#'
+#' @docType package
+#' @name beautier
+NULL
