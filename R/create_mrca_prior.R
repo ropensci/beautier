ba51c311340cba72330e615255ca3fd47328c892
richelbilderbeek <richel@richelbilderbeek.nl>
2018-07-07 19:38:33 +0200

    babette update

--- R/create_mrca_prior.R
+++ R/create_mrca_prior.R
@@ -8,6 +8,17 @@
 #'   Can be NA (the default) or any distribution
 #'   returned by \code{\link{create_distr}}
 #' @return an MRCA prior
+#' @examples
+#'  fasta_filename <- get_beautier_path("anthus_aco.fas")
+#'
+#'  # The first two taxa are sister species
+#'  mrca_prior <- create_mrca_prior(
+#'    alignment_id = get_alignment_id(fasta_filename = fasta_filename),
+#'    taxa_names = get_taxa_names(filename = fasta_filename)[1:2],
+#'    is_monophyletic = TRUE
+#'  )
+#'
+#'  #
 #' @author Richel J.C. Bilderbeek
 #' @export
 create_mrca_prior <- function(
