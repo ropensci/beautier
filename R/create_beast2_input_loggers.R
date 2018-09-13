55696d5013836d5ea593e05b1b11a2ad8a7787b3
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-13 10:39:15 +0200

    @lintr-bot

--- R/create_beast2_input_loggers.R
+++ R/create_beast2_input_loggers.R
@@ -59,12 +59,12 @@ create_beast2_input_loggers <- function( # nolint keep long function name, as it
 #' @examples
 #'   created <- beautier:::create_beast2_input_tracelog(ids = 1)
 #'   expected <- c(
-#'     "<logger id=\"tracelog\" fileName=\"1.log\" logEvery=\"1000\" model=\"@posterior\" sanitiseHeaders=\"true\" sort=\"smart\">",
+#'     "<logger id=\"tracelog\" fileName=\"1.log\" logEvery=\"1000\" model=\"@posterior\" sanitiseHeaders=\"true\" sort=\"smart\">",  # nolint
 #'     "    <log idref=\"posterior\"/>",
 #'     "    <log idref=\"likelihood\"/>",
 #'     "    <log idref=\"prior\"/>",
 #'     "    <log idref=\"treeLikelihood.1\"/>",
-#'     "    <log id=\"TreeHeight.t:1\" spec=\"beast.evolution.tree.TreeHeightLogger\" tree=\"@Tree.t:1\"/>",
+#'     "    <log id=\"TreeHeight.t:1\" spec=\"beast.evolution.tree.TreeHeightLogger\" tree=\"@Tree.t:1\"/>", # nolint
 #'     "    <log idref=\"YuleModel.t:1\"/>",
 #'     "    <log idref=\"birthRate.t:1\"/>",
 #'     "</logger>"
