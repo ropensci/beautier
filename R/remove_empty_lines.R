b9a24dd44cf8c277dff80e5181b95c0d8f6cdb37
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-22 13:11:49 +0200

    Add 'remove_empty_lines'

---
+++ R/remove_empty_lines.R
@@ -0,0 +1,9 @@
+#' Remove all lines that are only whitespace
+#' @param lines character vector with text
+#' @return the lines with text
+#' @author Richel J.C. Bilderbeek
+#' @noRd
+remove_empty_lines <- function(lines) {
+  trimmed <- stringr::str_trim(lines)
+  trimmed[stringr::str_length(trimmed) > 0]
+}
---
+++ tests/testthat/test-remove_empty_lines.R
@@ -0,0 +1,21 @@
+context("remove_empty_lines")
+
+test_that("use", {
+
+  expect_equal(
+    beautier:::remove_empty_lines(lines = c("A", "B")),
+    c("A", "B")
+  )
+  expect_equal(
+    beautier:::remove_empty_lines(lines = c("A", "", "B")),
+    c("A", "B")
+  )
+  expect_equal(
+    beautier:::remove_empty_lines(lines = c("A", " ", "B")),
+    c("A", "B")
+  )
+  expect_equal(
+    beautier:::remove_empty_lines(lines = c("A", "\t", "B")),
+    c("A", "B")
+  )
+})
