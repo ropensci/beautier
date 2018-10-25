2221ff4509f8ffc7294d4bb5d4710c01357de3d9
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-25 08:10:50 +0200

    Site model has the correct number of distributions depending on the gamma site model, progress #50

--- R/get_gamma_site_model_n_distrs.R
+++ R/get_gamma_site_model_n_distrs.R
@@ -2,16 +2,25 @@
 #' @inheritParams default_params_doc
 #' @return the number of distributrion a gamma site model has
 #' @examples
-#'   gamma_site_model <- create_gamma_site_model(prop_invariant = 0.5)
-#'
 #'   gamma_site_model <- create_gamma_site_model()
-#'   gamma_site_model_n_distrs <- get_gamma_site_model_n_distrs(
+#'   n_distrs <- get_gamma_site_model_n_distrs(
 #'     gamma_site_model
 #'    )
-#'   testit::assert(gamma_site_model_n_distrs == 1)
+#'   testit::assert(n_distrs == 0)
+#'
+#'   gamma_site_model <- create_gamma_site_model(
+#'     gamma_cat_count = 2,
+#'     gamma_shape_prior_distr = create_exp_distr()
+#'   )
+#'   n_distrs <- get_gamma_site_model_n_distrs(gamma_site_model)
+#'   testit::assert(n_distrs == 1)
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_gamma_site_model_n_distrs <- function(gamma_site_model) {
   testit::assert(is_gamma_site_model(gamma_site_model))
-  1
+  if (gamma_site_model$gamma_cat_count < 2) {
+    0
+  } else {
+    1
+  }
 }
--- tests/testthat/test-get_site_model_n_distrs.R
+++ tests/testthat/test-get_site_model_n_distrs.R
@@ -1,37 +1,84 @@
 context("get_site_model_n_distrs")
 
-test_that("use", {
+test_that("use, default gamma site model", {
 
-  # gamma_site_model$gamma_shape_prior_distr
   # rate_ac_prior_distr
   # rate_ag_prior_distr
   # rate_at_prior_distr
   # rate_cg_prior_distr
   # rate_gt_prior_distr
   testthat::expect_equal(
-    beautier:::get_site_model_n_distrs(create_gtr_site_model()),
-    6
+    beautier:::get_site_model_n_distrs(
+      create_gtr_site_model()),
+    5
   )
 
-  # gamma_site_model$gamma_shape_prior_distr
   # kappa_prior_distr
   testthat::expect_equal(
     beautier:::get_site_model_n_distrs(create_hky_site_model()),
-    2
-  )
-
-  # gamma_site_model$gamma_shape_prior_distr
-  # no other distributions
-  testthat::expect_equal(
-    beautier:::get_site_model_n_distrs(create_jc69_site_model()),
     1
   )
 
-  # gamma_site_model$gamma_shape_prior_distr
+  # no other distributions
+  testthat::expect_equal(
+    beautier:::get_site_model_n_distrs(create_jc69_site_model()),
+    0
+  )
+
   # kappa_1_prior_distr
   # kappa_2_prior_distr
   testthat::expect_equal(
     beautier:::get_site_model_n_distrs(create_tn93_site_model()),
+    2
+  )
+})
+
+test_that("use, gamma site model with 1 distr", {
+
+  gamma_site_model <- create_gamma_site_model(
+    gamma_cat_count = 2,
+    gamma_shape_prior_distr = create_normal_distr()
+  )
+
+  # rate_ac_prior_distr
+  # rate_ag_prior_distr
+  # rate_at_prior_distr
+  # rate_cg_prior_distr
+  # rate_gt_prior_distr
+  # gamma_site_model$gamma_shape_prior_distr
+  testthat::expect_equal(
+    beautier:::get_site_model_n_distrs(
+      create_gtr_site_model(
+        gamma_site_model = gamma_site_model
+      )),
+    6
+  )
+
+  # kappa_prior_distr
+  # gamma_site_model$gamma_shape_prior_distr
+  testthat::expect_equal(
+    beautier:::get_site_model_n_distrs(create_hky_site_model(
+      gamma_site_model = gamma_site_model
+    )),
+    2
+  )
+
+  # only:
+  # gamma_site_model$gamma_shape_prior_distr
+  testthat::expect_equal(
+    beautier:::get_site_model_n_distrs(create_jc69_site_model(
+      gamma_site_model = gamma_site_model
+    )),
+    1
+  )
+
+  # kappa_1_prior_distr
+  # kappa_2_prior_distr
+  # gamma_site_model$gamma_shape_prior_distr
+  testthat::expect_equal(
+    beautier:::get_site_model_n_distrs(create_tn93_site_model(
+      gamma_site_model = gamma_site_model
+    )),
     3
   )
 })
