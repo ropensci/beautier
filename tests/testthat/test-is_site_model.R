ec9b9c1bd6d6611399a27f804599d75a0892aa77
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 12:45:42 +0200

    Increase codecov

--- tests/testthat/test-is_site_model.R
+++ tests/testthat/test-is_site_model.R
@@ -205,3 +205,64 @@ test_that("is_hky_site_model: devious", {
   h$freq_equilibrium <- "nonsense"
   expect_false(is_hky_site_model(h))
 })
+
+test_that("is_tn93_site_model: devious", {
+
+  g <- create_tn93_site_model()
+  expect_true(is_tn93_site_model(g))
+
+  # No site model
+  h <- g
+  h <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # Invalid name
+  h <- g
+  h$name <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # No 'kappa_1_prior_distr'
+  h <- g[names(g) != "kappa_1_prior_distr"]
+  expect_false(is_tn93_site_model(h))
+
+  # Invalid 'kappa_1_prior_distr'
+  h <- g
+  h$kappa_1_prior_distr <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # No 'kappa_2_prior_distr'
+  h <- g[names(g) != "kappa_2_prior_distr"]
+  expect_false(is_tn93_site_model(h))
+
+  # Invalid 'kappa_2_prior_distr'
+  h <- g
+  h$kappa_2_prior_distr <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # No 'kappa_1_param'
+  h <- g[names(g) != "kappa_1_param"]
+  expect_false(is_tn93_site_model(h))
+
+  # Invalid 'kappa_1_param'
+  h <- g
+  h$kappa_1_param <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # No 'kappa_2_param'
+  h <- g[names(g) != "kappa_2_param"]
+  expect_false(is_tn93_site_model(h))
+
+  # Ivalid 'kappa_2_param'
+  h <- g
+  h$kappa_2_param <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+
+  # No 'freq_equilibrium'
+  h <- g[names(g) != "freq_equilibrium"]
+  expect_false(is_tn93_site_model(h))
+
+  # Invalid 'freq_equilibrium'
+  h <- g
+  h$freq_equilibrium <- "nonsense"
+  expect_false(is_tn93_site_model(h))
+})
