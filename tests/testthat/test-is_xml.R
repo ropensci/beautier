951ad29a955e512207da396242934cc9b911092c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 15:34:49 +0200

    Increase codecov

--- tests/testthat/test-is_xml.R
+++ tests/testthat/test-is_xml.R
@@ -2,14 +2,15 @@ context("is_xml")
 
 test_that("use", {
 
-  testthat::expect_false(beautier:::is_xml(NA))
-  testthat::expect_true(beautier:::is_xml("<text content=\"Hello\"/>"))
-  testthat::expect_true(beautier:::is_xml(
-    beautier:::parameter_to_xml(create_param_alpha(id = 1)))
+  expect_false(is_xml(NA))
+  expect_true(is_xml("<text content=\"Hello\"/>"))
+  expect_true(is_xml(
+    parameter_to_xml(create_param_alpha(id = 1)))
   )
-  testthat::expect_true(beautier:::is_xml(
-    beautier:::distr_to_xml(create_one_div_x_distr(id = 1)))
+  expect_true(is_xml(
+    distr_to_xml(create_one_div_x_distr(id = 1)))
   )
-  testthat::expect_false(beautier:::is_xml("<text id=1></invalid_close>"))
-  testthat::expect_false(beautier:::is_xml("<text content=\"Hello\">"))
+  expect_false(is_xml("no opening tag"))
+  expect_false(is_xml("<text id=1></invalid_close>"))
+  expect_false(is_xml("<text content=\"Hello\">"))
 })
