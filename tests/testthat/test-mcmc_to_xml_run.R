7dbbcff5201a766eb2cb4be6c7dbf898c3743cb8
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 15:50:44 +0200

    Increase codecov

--- tests/testthat/test-mcmc_to_xml_run.R
+++ tests/testthat/test-mcmc_to_xml_run.R
@@ -2,16 +2,36 @@ context("mcmc_to_xml_run")
 
 test_that("use, default", {
 
-  created <- beautier:::mcmc_to_xml_run(create_mcmc())
+  created <- mcmc_to_xml_run(create_mcmc())
   expected <- c("<run id=\"mcmc\" spec=\"MCMC\" chainLength=\"10000000\">")
-  testthat::expect_equal(created, expected)
+  expect_equal(created, expected)
 
 })
 
 test_that("use, 1K", {
 
-  created <- beautier:::mcmc_to_xml_run(create_mcmc(chain_length = 1000))
+  created <- mcmc_to_xml_run(create_mcmc(chain_length = 1000))
   expected <- c("<run id=\"mcmc\" spec=\"MCMC\" chainLength=\"1000\">")
-  testthat::expect_equal(created, expected)
+  expect_equal(created, expected)
+
+})
+
+test_that("use, mcmc_to_xml_run, 1K", {
+
+  created <- mcmc_to_xml_run(create_mcmc_nested_sampling(chain_length = 1000))
+  expected <- c(
+    "<run id=\"mcmc\" spec=\"beast.gss.NS\" chainLength=\"1000\" particleCount=\"1\" subChainLength=\"5000\" epsilon=\"1e-12\">" # nolint indeed a long string
+  )
+  expect_equal(created, expected)
+
+})
+
+test_that("use, mcmc_to_xml_run, store every", {
+
+  created <- mcmc_to_xml_run(create_mcmc_nested_sampling(chain_length = 10000, store_every = 1000))
+  expected <- c(
+    "<run id=\"mcmc\" spec=\"beast.gss.NS\" chainLength=\"10000\" storeEvery=\"1000\" particleCount=\"1\" subChainLength=\"5000\" epsilon=\"1e-12\">" # nolint indeed a long string
+  )
+  expect_equal(created, expected)
 
 })
