54b4b120ee378035b7ac72abe22086018fa4ea8a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 12:35:54 +0200

    Increase codecov

--- tests/testthat/test-is_site_model.R
+++ tests/testthat/test-is_site_model.R
@@ -12,11 +12,10 @@ test_that("use", {
 
 })
 
-test_that("devious", {
+test_that("is_site_model: devious", {
 
   g <- create_jc69_site_model()
   expect_true(is_site_model(g))
-
 
   # No 'name'
   h <- g[names(g) != "name"]
@@ -39,4 +38,117 @@ test_that("devious", {
   h <- g
   h$gamma_site_model <- "nonsense"
   expect_false(is_site_model(h))
+})
+
+test_that("is_gtr_site_model: devious", {
+
+  g <- create_gtr_site_model()
+  expect_true(is_gtr_site_model(g))
+
+  # No site model
+  h <- g
+  h <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  # Invalid name
+  h <- g
+  h$name <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_ac_prior_distr'
+  h <- g[names(g) != "rate_ac_prior_distr"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_ag_prior_distr'
+  h <- g[names(g) != "rate_ag_prior_distr"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_at_prior_distr'
+  h <- g[names(g) != "rate_at_prior_distr"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_cg_prior_distr'
+  h <- g[names(g) != "rate_cg_prior_distr"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_gt_prior_distr'
+  h <- g[names(g) != "rate_gt_prior_distr"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_ac_param'
+  h <- g[names(g) != "rate_ac_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_ag_param'
+  h <- g[names(g) != "rate_ag_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_at_param'
+  h <- g[names(g) != "rate_at_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_cg_param'
+  h <- g[names(g) != "rate_cg_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_ct_param'
+  h <- g[names(g) != "rate_ct_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'rate_gt_param'
+  h <- g[names(g) != "rate_gt_param"]
+  expect_false(is_gtr_site_model(h))
+
+  # No 'freq_equilibrium'
+  h <- g[names(g) != "freq_equilibrium"]
+  expect_false(is_gtr_site_model(h))
+
+  # Invalid distributions
+  h <- g
+  h$rate_ac_prior_distr <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_ag_prior_distr <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_at_prior_distr <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_cg_prior_distr <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_gt_prior_distr <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_ac_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_ag_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_at_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_cg_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_ct_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$rate_gt_param <- "nonsense"
+  expect_false(is_gtr_site_model(h))
+
+  h <- g
+  h$freq_equilibrium <- "nonsense"
+  expect_false(is_gtr_site_model(h))
 })
