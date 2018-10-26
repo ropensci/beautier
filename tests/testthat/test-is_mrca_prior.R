9430508d1e17b6a15ce24cbe28c40a941b3afa33
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 12:12:26 +0200

    Increase codecov

--- tests/testthat/test-is_mrca_prior.R
+++ tests/testthat/test-is_mrca_prior.R
@@ -10,12 +10,12 @@ test_that("use", {
     mrca_distr = create_normal_distr()
   )
 
-  testthat::expect_true(is_mrca_prior(mrca_prior))
-  testthat::expect_true(is_mrca_prior(NA))
+  expect_true(is_mrca_prior(mrca_prior))
+  expect_true(is_mrca_prior(NA))
 
-  testthat::expect_false(is_mrca_prior("nonsense"))
-  testthat::expect_false(is_mrca_prior(42))
-  testthat::expect_false(is_mrca_prior(NULL))
+  expect_false(is_mrca_prior("nonsense"))
+  expect_false(is_mrca_prior(42))
+  expect_false(is_mrca_prior(NULL))
 })
 
 test_that("use, two", {
@@ -28,8 +28,49 @@ test_that("use, two", {
   )
   mrca_priors <- list(mrca_prior, mrca_prior)
 
-  testthat::expect_false(is_mrca_prior(mrca_priors))
-  testthat::expect_true(is_mrca_prior(mrca_priors[[1]]))
-  testthat::expect_true(is_mrca_prior(mrca_priors[[2]]))
+  expect_false(is_mrca_prior(mrca_priors))
+  expect_true(is_mrca_prior(mrca_priors[[1]]))
+  expect_true(is_mrca_prior(mrca_priors[[2]]))
 
+})
+
+test_that("devious", {
+
+  fasta_filename <- get_beautier_path("anthus_aco_sub.fas")
+  g <- create_mrca_prior(
+    name = "my_mrca_prior_name",
+    alignment_id = get_alignment_id(fasta_filename),
+    taxa_names = get_taxa_names(fasta_filename),
+    mrca_distr = create_normal_distr()
+  )
+  expect_true(beautier:::is_mrca_prior(g))
+
+  # No 'name'
+  h <- g[names(g) != "name"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # No 'alignment_id'
+  h <- g[names(g) != "alignment_id"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # No 'taxa_names'
+  h <- g[names(g) != "taxa_names"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # No 'is_monophyletic'
+  h <- g[names(g) != "is_monophyletic"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # No 'mrca_distr'
+  h <- g[names(g) != "mrca_distr"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # No 'clock_prior_distr_id'
+  h <- g[names(g) != "clock_prior_distr_id"]
+  expect_false(beautier:::is_mrca_prior(h))
+
+  # mrca_distr is not a distribution
+  h <- g
+  h$mrca_distr <- "nonsense"
+  expect_false(beautier:::is_mrca_prior(h))
 })
