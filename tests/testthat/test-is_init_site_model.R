3136c87bbc4a1f125b607f5554ef25b1dca8a883
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 14:36:43 +0200

    Increase codecov

--- tests/testthat/test-is_init_site_model.R
+++ tests/testthat/test-is_init_site_model.R
@@ -1,9 +1,101 @@
 context("is_init_site_model")
 
-test_that("hky site model", {
+test_that("is_init_site_model", {
 
-  testthat::expect_true(
-    beautier:::is_init_site_model(
+  expect_false(is_init_site_model("nonsense"))
+  expect_false(is_init_gtr_site_model("nonsense"))
+})
+
+test_that("is_init_gtr_site_model", {
+
+  g <- create_gtr_site_model(
+    id = 42,
+    gamma_site_model = create_gamma_site_model(
+      gamma_cat_count = 2,
+      gamma_shape_prior_distr = create_one_div_x_distr(id = 0)
+    ),
+    rate_ac_prior_distr = create_one_div_x_distr(id = 1),
+    rate_ag_prior_distr = create_one_div_x_distr(id = 2),
+    rate_at_prior_distr = create_one_div_x_distr(id = 3),
+    rate_cg_prior_distr = create_one_div_x_distr(id = 4),
+    rate_gt_prior_distr = create_one_div_x_distr(id = 6),
+    rate_ac_param = create_rate_ac_param(id = 7),
+    rate_ag_param = create_rate_ac_param(id = 8),
+    rate_at_param = create_rate_ac_param(id = 9),
+    rate_cg_param = create_rate_ac_param(id = 10),
+    rate_ct_param = create_rate_ac_param(id = 11),
+    rate_gt_param = create_rate_ac_param(id = 12)
+  )
+  testit::assert(is_init_gtr_site_model(g))
+
+  # Uninitialize 'rate_ac_prior_distr'
+  h <- g
+  h$rate_ac_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_ag_prior_distr'
+  h <- g
+  h$rate_ag_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_at_prior_distr'
+  h <- g
+  h$rate_at_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_cg_prior_distr'
+  h <- g
+  h$rate_cg_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # No 'rate_cg_prior_distr' yet
+
+  # Uninitialize 'rate_gt_prior_distr'
+  h <- g
+  h$rate_gt_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_ac_param'
+  h <- g
+  h$rate_ac_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_ag_param'
+  h <- g
+  h$rate_ag_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_at_param'
+  h <- g
+  h$rate_at_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_cg_param'
+  h <- g
+  h$rate_cg_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_ct_param'
+  h <- g
+  h$rate_ct_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'rate_gt_param'
+  h <- g
+  h$rate_gt_param$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+
+  # Uninitialize 'gamma_site_model'
+  h <- g
+  h$gamma_site_model$gamma_shape_prior_distr$id <- NA
+  expect_false(is_init_gtr_site_model(h))
+})
+
+
+test_that("is_init_site_model on HKY site model", {
+
+  expect_true(
+    is_init_site_model(
       create_hky_site_model(
         kappa_prior_distr = create_log_normal_distr(id = 1,
           m = create_m_param(id = 1),
@@ -17,8 +109,8 @@ test_that("hky site model", {
     )
   )
 
-  testthat::expect_false(
-    beautier:::is_init_site_model(
+  expect_false(
+    is_init_site_model(
       create_hky_site_model(
         kappa_prior_distr = create_log_normal_distr(id = NA,
             m = create_m_param(id = 1),
@@ -32,8 +124,8 @@ test_that("hky site model", {
     )
   )
 
-  testthat::expect_false(
-    beautier:::is_init_site_model(
+  expect_false(
+    is_init_site_model(
       create_hky_site_model(
         kappa_prior_distr = create_log_normal_distr(id = 1,
             m = create_m_param(id = NA),
@@ -47,8 +139,8 @@ test_that("hky site model", {
     )
   )
 
-  testthat::expect_false(
-    beautier:::is_init_site_model(
+  expect_false(
+    is_init_site_model(
       create_hky_site_model(
         kappa_prior_distr = create_log_normal_distr(id = 1,
             m = create_m_param(id = 1),
@@ -62,8 +154,8 @@ test_that("hky site model", {
     )
   )
 
-  testthat::expect_false(
-    beautier:::is_init_site_model(
+  expect_false(
+    is_init_site_model(
       create_hky_site_model(
         kappa_prior_distr = create_log_normal_distr(id = 1,
           m = create_m_param(id = 1),
