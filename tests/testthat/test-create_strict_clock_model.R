d3c10cf635e4c331c16c265fdb57b36c99b2a6b0
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 14:23:39 +0200

    Increase codecov

--- tests/testthat/test-create_strict_clock_model.R
+++ tests/testthat/test-create_strict_clock_model.R
@@ -2,21 +2,39 @@ context("create_strict_clock_model")
 
 test_that("use", {
 
-  testthat::expect_true(
+  expect_true(
     is_strict_clock_model(
       create_strict_clock_model()
     )
   )
+
+  expect_true(
+    is_strict_clock_model(
+      create_strict_clock_model(
+        clock_rate_param = create_clock_rate_param(value = 3.14)
+      )
+    )
+  )
+
+  # Simplified interface
+  expect_true(
+    is_strict_clock_model(
+      create_strict_clock_model(
+        clock_rate_param = 3.14
+      )
+    )
+  )
+
 })
 
 test_that("abuse", {
 
-  testthat::expect_error(
+  expect_error(
     create_strict_clock_model(clock_rate_param = NA),
     "'clock_rate_param' must be a clock rate parameter"
   )
 
-  testthat::expect_error(
+  expect_error(
     create_strict_clock_model(clock_rate_distr = NA),
     "'clock_rate_distr' must be a distribution"
   )
