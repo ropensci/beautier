357b80bc73ea6827cbaf0ce177f88eb1c2b28e65
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 12:17:06 +0200

    Increase codecov

--- tests/testthat/test-is_mcmc.R
+++ tests/testthat/test-is_mcmc.R
@@ -2,23 +2,58 @@ context("is_mcmc")
 
 test_that("is_mcmc, use", {
 
-  testthat::expect_true(is_mcmc(create_mcmc()))
-  testthat::expect_true(is_mcmc(create_mcmc_nested_sampling()))
-  testthat::expect_false(is_mcmc("nonsense"))
-  testthat::expect_false(is_mcmc(NA))
-  testthat::expect_false(is_mcmc(NULL))
-  testthat::expect_false(is_mcmc(list(chain_length = 0)))
+  expect_true(is_mcmc(create_mcmc()))
+  expect_true(is_mcmc(create_mcmc_nested_sampling()))
+  expect_false(is_mcmc("nonsense"))
+  expect_false(is_mcmc(NA))
+  expect_false(is_mcmc(NULL))
+  expect_false(is_mcmc(list(chain_length = 0)))
 
+})
+
+test_that("ismcmc: devious", {
+
+  g <- create_mcmc()
+  expect_true(beautier:::is_mcmc(g))
+
+  # No 'chain_length'
+  h <- g[names(g) != "chain_length"]
+  expect_false(beautier:::is_mcmc(h))
+
+  # Invalid 'chain_length'
+  h <- g
+  h$chain_length <- -123
+  expect_false(beautier:::is_mcmc(h))
+
+  # No 'store_every'
+  h <- g[names(g) != "store_every"]
+  expect_false(beautier:::is_mcmc(h))
+
+  # Invalid 'store_every': below -1
+  h <- g
+  h$store_every <- -123
+  expect_false(beautier:::is_mcmc(h))
+
+  # Invalid 'store_every': at zero
+  h <- g
+  h$store_every <- 0
+  expect_false(beautier:::is_mcmc(h))
+
+  # 'store_every' bigger than chain length
+  h <- g
+  h$store_every <- 100000
+  h$chain_length <- 10
+  expect_false(beautier:::is_mcmc(h))
 })
 
 test_that("is_mcmc_nested_sampling, use", {
 
-  testthat::expect_true(
+  expect_true(
     beautier:::is_mcmc_nested_sampling(create_mcmc_nested_sampling())
   )
-  testthat::expect_false(beautier:::is_mcmc_nested_sampling(create_mcmc()))
-  testthat::expect_false(is_mcmc_nested_sampling("nonsense"))
-  testthat::expect_false(is_mcmc_nested_sampling(NA))
-  testthat::expect_false(is_mcmc_nested_sampling(NULL))
-  testthat::expect_false(is_mcmc_nested_sampling(list(chain_length = 0)))
+  expect_false(beautier:::is_mcmc_nested_sampling(create_mcmc()))
+  expect_false(is_mcmc_nested_sampling("nonsense"))
+  expect_false(is_mcmc_nested_sampling(NA))
+  expect_false(is_mcmc_nested_sampling(NULL))
+  expect_false(is_mcmc_nested_sampling(list(chain_length = 0)))
 })
