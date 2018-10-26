072f80030661cab821be0d455e007914884cf53f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 15:33:50 +0200

    Increase codecov

--- tests/testthat/test-create_mcmc_nested_sampling.R
+++ tests/testthat/test-create_mcmc_nested_sampling.R
@@ -6,3 +6,21 @@ test_that("use", {
   expect_false(is_default_mcmc("nonsense"))
   expect_true(is_mcmc_nested_sampling(create_mcmc_nested_sampling()))
 })
+
+test_that("abuse", {
+  expect_silent(
+    create_mcmc_nested_sampling()
+  )
+  expect_error(
+    create_mcmc_nested_sampling(particle_count = 0),
+    "'particle_count' must be a non-zero amount"
+  )
+  expect_error(
+    create_mcmc_nested_sampling(sub_chain_length = 0),
+    "'sub_chain_length' must be a non-zero amount"
+  )
+  expect_error(
+    create_mcmc_nested_sampling(epsilon = 0.0),
+    "'epsilon' must be a non-zero number"
+  )
+})
