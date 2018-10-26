b7d1f001ba921fc43183c5c49142b7e80a907f5f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-26 12:07:46 +0200

    Increase codecov

--- tests/testthat/test-is_gamma_site_model.R
+++ tests/testthat/test-is_gamma_site_model.R
@@ -2,11 +2,58 @@ context("is_gamma_site_model")
 
 test_that("use", {
 
-  testthat::expect_true(beautier:::is_gamma_site_model(
+  expect_true(beautier:::is_gamma_site_model(
     create_gamma_site_model())
   )
-  testthat::expect_false(beautier:::is_gamma_site_model("nonsense"))
-  testthat::expect_false(beautier:::is_gamma_site_model(NA))
-  testthat::expect_false(beautier:::is_gamma_site_model(NULL))
 
+  expect_false(beautier:::is_gamma_site_model("nonsense"))
+  expect_false(beautier:::is_gamma_site_model(NA))
+  expect_false(beautier:::is_gamma_site_model(NULL))
+})
+
+test_that("devious", {
+
+  g <- create_gamma_site_model()
+  expect_true(beautier:::is_gamma_site_model(g))
+
+  # No gamma_cat_count
+  h <- g[names(g) != "gamma_cat_count"]
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # Invalid gamma_cat_count
+  h <- create_gamma_site_model()
+  h$gamma_cat_count <- -123
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # No gamma_shape
+  h <- g[names(g) != "gamma_shape"]
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # Invalid gamma shaoe
+  h <- create_gamma_site_model()
+  h$gamma_shape <- -123
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # No prop_invariant
+  h <- g[names(g) != "prop_invariant"]
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # prop_invariant below zero
+  h <- create_gamma_site_model()
+  h$prop_invariant <- -123.456
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # prop_invariant above one
+  h <- create_gamma_site_model()
+  h$prop_invariant <- 123.456
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # No gamma_shape_prior_distr
+  h <- g[names(g) != "gamma_shape_prior_distr"]
+  expect_false(beautier:::is_gamma_site_model(h))
+
+  # Having a distribution, that is no distribution
+  h <- create_gamma_site_model()
+  h$gamma_shape_prior_distr <- "nonsense"
+  expect_false(beautier:::is_gamma_site_model(h))
 })
