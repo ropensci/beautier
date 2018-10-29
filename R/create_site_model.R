ff73d0f07259d50b4d3fbbc7f744c6dd87f1fdae
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-29 06:59:05 +0100

    Simplified default usage

--- R/create_site_model.R
+++ R/create_site_model.R
@@ -294,12 +294,12 @@ create_tn93_site_model <- create_site_model_tn93 <- function(
   kappa_1_param = create_kappa_1_param(),
   kappa_2_param = create_kappa_2_param(),
   kappa_1_prior_distr = create_log_normal_distr(
-    m = create_m_param(id = NA, value = "1.0"),
-    s = create_s_param(id = NA, value = "1.25")
+    m = 1.0,
+    s = 1.25
   ),
   kappa_2_prior_distr = create_log_normal_distr(
-    m = create_m_param(id = NA, value = "1.0"),
-    s = create_s_param(id = NA, value = "1.25")
+    m = 1.0,
+    s = 1.25
   ),
   freq_equilibrium = "estimated"
 ) {
