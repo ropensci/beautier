3c22952af6f6f0c45224d90ac87401dd78dfd220
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-11 09:28:51 +0100

    Move 'create_random_' functions to babetter

--- R/create_random_name.R
+++
@@ -1,9 +0,0 @@
-#' Create a random name, for example,
-#'   to be used by \code{\link{create_mrca_prior}}
-#' @inheritParams default_params_doc
-#' @return a random name
-#' @author Richel J.C. Bilderbeek
-#' @noRd
-create_random_name <- function() {
-  paste0(sample(x = letters, size = 42, replace = TRUE), collapse = "")
-}
