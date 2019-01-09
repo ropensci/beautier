37bf7b1cce86179bfc7c5bd788e6b88c342b95fc
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-09 08:56:01 +0100

    Improve 'check_site_model(s)', and its doc

--- R/check_site_models.R
+++ R/check_site_models.R
@@ -29,7 +29,7 @@ check_site_models <- function(site_models) {
   if (!are_site_models(site_models)) { # nolint internal function
     stop(
       "'site_models' must be a list of one or more valid site models. ",
-      "Actual value(s): ", x
+      "Actual value(s): ", site_models
     )
   }
 
