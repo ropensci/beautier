9b4fe02f55512ca183b9aab57e4fea6579e7fd61
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-15 13:00:21 +0100

    Fix doc

--- R/check_mrca_prior.R
+++ R/check_mrca_prior.R
@@ -14,7 +14,7 @@
 #'    alignment_id = get_alignment_id(fasta_filename = fasta_filename),
 #'    taxa_names = get_taxa_names(filename = fasta_filename)
 #'   )
-#'   expect_silent(check_mrca_prior(mrca_prior))
+#'   testthat::expect_silent(check_mrca_prior(mrca_prior))
 #'
 #'   # NA is a valid MRCA prior
 #'   testthat::expect_silent(check_mrca_prior(mrca_prior = NA))
