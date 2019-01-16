cbc87409b6c403b4aa636644a48b5e282cbb2dca
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-16 10:47:42 +0100

    Export 'is_phylo'

--- R/is_phylo.R
+++ R/is_phylo.R
@@ -1,8 +1,15 @@
 #' Checks if the input is a phylogeny
 #' @param x input to be checked
 #' @return TRUE or FALSE
+#' @examples
+#'   phylogeny <- ape::read.tree(text = "(a:15,b:15):1;")
+#'   testit::assert(is_phylo(phylogeny))
+#'
+#'   testit::assert(!is_phylo("nonsense"))
+#'   testit::assert(!is_phylo(NA))
+#'   testit::assert(!is_phylo(NULL))
 #' @author Richel J.C. Bilderbeek
-#' @noRd
+#' @export
 is_phylo <- function(x) {
   class(x) == "phylo"
 }
