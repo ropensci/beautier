02b2111fdc422f596728d2f8a2767f87b6540416
richelbilderbeek <richel@richelbilderbeek.nl>
2019-05-23 11:10:06 +0200

    Use functions that actually exist

--- tests/testthat/test-create_beast2_input_package.R
+++ tests/testthat/test-create_beast2_input_package.R
@@ -6,32 +6,33 @@ context("create_beast2_input_package")
 # NS: nested sampling
 ################################################################################
 test_that("NS, shallow", {
-  normal <- create_beast2_input_from_model(
-  input_filename = get_beautier_path("anthus_aco.fas"),
-  inference_model = create_inference_model(
-      mcmc = create_mcmc_nested_sampling()
-    )
+  normal <- create_beast2_input(
+    input_filename = get_beautier_path("anthus_aco.fas"),
+    mcmc = create_mcmc_nested_sampling()
   )
   matches <- stringr::str_match(string = normal, pattern = ".*run id=.*")
-  line <- matches[!is.na(matches)]
-  testit::assert(line == "<run id=\"mcmc\" spec=\"beast.gss.NS\" chainLength=\"10000000\" particleCount=\"1\" subChainLength=\"5000\" epsilon=\"1e-12\">") # nolint
+  expect_equal(
+    matches[!is.na(matches)],
+    "<run id=\"mcmc\" spec=\"beast.gss.NS\" chainLength=\"10000000\" particleCount=\"1\" subChainLength=\"5000\" epsilon=\"1e-12\">" # nolint long line indeed
+  )
 })
 
 test_that("NS, deep", {
 
-  return()
 
-  # Cannot check a BEAST2 input file to be valid
-  # when it is using a construct of the NS package
-  output_filename <- tempfile()
+  output_filename <- tempfile(pattern = "beast2_input_file_", fileext = ".xml")
 
   create_beast2_input_file_from_model(
-    input_filename = get_beautier_path("anthus_aco.fas"),
+    input_filename = get_beautier_path("anthus_aco_sub.fas"),
     create_inference_model(
       mcmc = create_mcmc_nested_sampling()
     ),
     output_filename = output_filename
   )
+
+  # Cannot check a BEAST2 input file to be valid
+  # when it is using a construct of the NS package
+  return()
   expect_false(
     beastier::is_beast2_input_file(
       output_filename,
