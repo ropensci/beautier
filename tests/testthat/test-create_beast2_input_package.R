6082790366597e3b25c6286298e2bc41fb244201
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-06 09:42:37 +0200

    Move (comments of) deep tests that require beastier 'test-create_beast2_input_package' to beastier

--- tests/testthat/test-create_beast2_input_package.R
+++ tests/testthat/test-create_beast2_input_package.R
@@ -1,7 +1,6 @@
 context("create_beast2_input_package")
 
 # Create BEAST2 input files that employ a BEAST2 package
-
 ################################################################################
 # NS: nested sampling
 ################################################################################
@@ -19,25 +18,8 @@ test_that("NS, shallow", {
 
 test_that("NS, deep", {
 
-
-  output_filename <- tempfile(pattern = "beast2_input_file_", fileext = ".xml")
-
-  create_beast2_input_file_from_model(
-    input_filename = get_beautier_path("anthus_aco_sub.fas"),
-    create_inference_model(
-      mcmc = create_mcmc_nested_sampling()
-    ),
-    output_filename = output_filename
-  )
-
-  # Cannot check a BEAST2 input file to be valid
-  # when it is using a construct of the NS package
-  return()
-  expect_false(
-    beastier::is_beast2_input_file(
-      output_filename,
-      show_warnings = FALSE,
-      verbose = TRUE
-    )
-  )
+  # We cannot deeply test if the BEAST2 input file is valid:
+  # for that, we need beastier.
+  # Therefore, this test is done in
+  # beastier's tests/testthat/test-create_beast2_input_package.R
 })
