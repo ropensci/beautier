5ce516396b6147b5506b94715136e200292ba77a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-10 12:58:41 +0200

    Simplify 'check_mcmc_nested_sampling'

--- R/check_mcmc_nested_sampling.R
+++ R/check_mcmc_nested_sampling.R
@@ -8,15 +8,12 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 check_mcmc_nested_sampling <- function(mcmc) {
-  if (!is_mcmc(mcmc)) { # nolint beautier function
-    stop(
-      "'mcmc' must an MCMC that uses Nested Sampling. \n",
-      "Tip: use 'create_nested_sampling_mcmc'"
-    )
-  }
+  check_mcmc(mcmc) # nolint beautier function
+
+  # The arguments 'chain_length' and 'store_every' are checked
+  # by check_mcmc
   argument_names <- c(
-    "chain_length", "store_every", "particle_count", "sub_chain_length",
-    "epsilon"
+    "particle_count", "sub_chain_length", "epsilon"
   )
   for (arg_name in argument_names) {
     if (!arg_name %in% names(mcmc)) {
@@ -27,12 +24,9 @@ check_mcmc_nested_sampling <- function(mcmc) {
       )
     }
   }
-  if (mcmc$chain_length < 1) {
-    stop("'mcmc$chain_length' must be at least 1")
-  }
-  if (mcmc$store_every == 0 || mcmc$store_every < -1) {
-    stop("'mcmc$store_every' must be -1 or at least 1")
-  }
+
+  # The arguments 'chain_length' and 'store_every' are checked
+  # by check_mcmc
   if (mcmc$particle_count < 1) {
     stop("'mcmc$particle_count' must be at least 1")
   }
