951d55e233655b6a245cc47e5fb1b17405683af4
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-10 10:20:12 +0200

    'is_site_model_name' is exported

--- R/is_site_model_name.R
+++ R/is_site_model_name.R
@@ -2,12 +2,15 @@
 #' @param name the name to be tested
 #' @return TRUE if the name is a valid site_model name, FALSE otherwise
 #' @examples
-#'   testit::assert(beautier:::is_site_model_name("JC69"))
-#'   testit::assert(beautier:::is_site_model_name("HKY"))
-#'   testit::assert(beautier:::is_site_model_name("TN93"))
-#'   testit::assert(beautier:::is_site_model_name("GTR"))
+#' library(testthat)
+#'
+#' expect_true(is_site_model_name("JC69"))
+#' expect_true(is_site_model_name("HKY"))
+#' expect_true(is_site_model_name("TN93"))
+#' expect_true(is_site_model_name("GTR"))
+#' expect_false(is_site_model_name("nonsense"))
 #' @author Richèl J.C. Bilderbeek
-#' @noRd
+#' @export
 is_site_model_name <- function(name) {
   name %in% get_site_model_names() # nolint beautier function
 }
