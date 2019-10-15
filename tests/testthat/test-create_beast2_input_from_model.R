fe1577fd02134eff443a3ec52e759628b4e2d20e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-15 13:45:56 +0200

    Expose #94

--- tests/testthat/test-create_beast2_input_from_model.R
+++ tests/testthat/test-create_beast2_input_from_model.R
@@ -1,6 +1,38 @@
-test_that("use", {
-  expect_equal(2 * 2, 4)
-  create_beast2_input_from_model
-  create_beast2_input_file_from_model
+test_that("use, MCMC store every of 2k", {
+  if (!beastier::is_beast2_installed()) return()
 
+  inference_model <- create_inference_model(
+    mcmc = create_mcmc(chain_length = 6000, store_every = 2000)
+  )
+  text <- create_beast2_input_from_model(
+    input_filename = get_beautier_path("anthus_aco.fas"),
+    inference_model = inference_model
+  )
+  store_every <- as.numeric(
+    na.omit(
+      stringr::str_match(
+        string = text,
+        pattern = ".*MCMC.*storeEvery=.(.*).>"
+      )[, 2]
+    )
+  )
+  expect_equal(
+    inference_model$mcmc$store_every,
+    store_every
+  )
+
+  skip("Expose #94")
+
+  log_every <- as.numeric(
+    na.omit(
+      stringr::str_match(
+        string = text,
+        pattern = "treelog.*logEvery..(.*). "
+      )[, 2]
+    )
+  )
+  expect_equal(
+    inference_model$mcmc$store_every,
+    log_every
+  )
 })
