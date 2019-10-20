a534a1ac2a7421a2d48181110a0d79bf6fcd59f6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-20 15:17:48 +0200

    Add 'pre_burnin' to 'create_mcmc', expose #98

--- tests/testthat/test-create_beast2_input_from_model.R
+++ tests/testthat/test-create_beast2_input_from_model.R
@@ -35,3 +35,42 @@ test_that("use, MCMC store every of 2k", {
     log_every
   )
 })
+
+test_that("mcmc$pre_burning", {
+
+  inference_model <- create_inference_model(
+    mcmc = create_mcmc(
+      chain_length = 1e7,
+      store_every = 1e4,
+      pre_burnin = 1e6
+    )
+  )
+  text <- create_beast2_input_from_model(
+    input_filename = get_fasta_filename(),
+    inference_model = inference_model
+  )
+  skip("Expose #98")
+  the_line <- as.character(
+    na.omit(
+      stringr::str_match(
+        string = text,
+        pattern = ".*MCMC.*"
+      )[, 1]
+    )
+  )
+  the_line
+  pre_burnin <- as.numeric(
+    na.omit(
+      stringr::str_match(
+        string = text,
+        pattern = ".*MCMC.*preBurnin=.(.*).>"
+      )[, 2]
+    )
+  )
+  pre_burnin
+  expect_equal(
+    inference_model$mcmc$pre_burnin,
+    pre_burnin
+  )
+})
+
