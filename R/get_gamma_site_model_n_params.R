03c26e707e5d16a28ea19c2b9d087bb2aa353e0c
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-20 19:50:11 +0200

    Thanks @lintr-bot

--- R/get_gamma_site_model_n_params.R
+++ R/get_gamma_site_model_n_params.R
@@ -4,31 +4,31 @@
 #' @author Richèl J.C. Bilderbeek
 #' @examples
 #'   testit::assert(
-#'     beautier:::get_gamma_site_model_n_params(
+#'     get_gamma_site_model_n_params(
 #'       create_gamma_site_model(gamma_cat_count = 0)
 #'     ) == 0
 #'   )
 #'   testit::assert(
-#'     beautier:::get_gamma_site_model_n_params(
+#'     get_gamma_site_model_n_params(
 #'       create_gamma_site_model(gamma_cat_count = 1)
 #'     ) == 0
 #'   )
 #'   testit::assert(
-#'     beautier:::get_gamma_site_model_n_params(
+#'     get_gamma_site_model_n_params(
 #'       create_gamma_site_model(
 #'         gamma_cat_count = 2,
 #'         gamma_shape_prior_distr = create_exp_distr()
 #'       )
 #'     ) == 1
 #'   )
-#' @noRd
+#' @export
 get_gamma_site_model_n_params <- function(
   gamma_site_model
 ) {
-  testit::assert(is_gamma_site_model(gamma_site_model)) # nolint beautier function
+  testit::assert(beautier::is_gamma_site_model(gamma_site_model))
   if (gamma_site_model$gamma_cat_count < 2) {
     0
   } else {
-    get_distr_n_params(gamma_site_model$gamma_shape_prior_distr) # nolint beautier function
+    beautier::get_distr_n_params(gamma_site_model$gamma_shape_prior_distr)
   }
 }
--- R/get_site_model_n_params.R
+++ R/get_site_model_n_params.R
@@ -4,53 +4,53 @@
 #' @author Richèl J.C. Bilderbeek
 #' @examples
 #'   testit::assert(
-#'     beautier:::get_site_model_n_params(create_gtr_site_model()) == 11
+#'     get_site_model_n_params(create_gtr_site_model()) == 10
 #'   )
 #'   testit::assert(
-#'     beautier:::get_site_model_n_params(create_hky_site_model()) == 3
+#'     get_site_model_n_params(create_hky_site_model()) == 2
 #'   )
 #'   testit::assert(
-#'     beautier:::get_site_model_n_params(create_jc69_site_model()) == 1
+#'     get_site_model_n_params(create_jc69_site_model()) == 0
 #'   )
 #'   testit::assert(
-#'     beautier:::get_site_model_n_params(create_tn93_site_model()) == 5
+#'     get_site_model_n_params(create_tn93_site_model()) == 4
 #'   )
-#' @noRd
+#' @export
 get_site_model_n_params <- function(
   site_model
 ) {
-  if (!is_site_model(site_model)) { # nolint beautier function
+  if (!beautier::is_site_model(site_model)) {
     stop("'site_model' must be a site model")
   }
-  gamma_site_model_n_params <- get_gamma_site_model_n_params( # nolint beautier function
+  gamma_site_model_n_params <- beautier::get_gamma_site_model_n_params(
     site_model$gamma_site_model
   )
 
-  if (is_gtr_site_model(site_model)) { # nolint beautier function
+  if (beautier::is_gtr_site_model(site_model)) {
     return(
       gamma_site_model_n_params +
-      get_distr_n_params(site_model$rate_ac_prior_distr) + # nolint beautier function
-      get_distr_n_params(site_model$rate_ag_prior_distr) + # nolint beautier function
-      get_distr_n_params(site_model$rate_at_prior_distr) + # nolint beautier function
-      get_distr_n_params(site_model$rate_cg_prior_distr) + # nolint beautier function
-      get_distr_n_params(site_model$rate_gt_prior_distr) # nolint beautier function
+      beautier::get_distr_n_params(site_model$rate_ac_prior_distr) +
+      beautier::get_distr_n_params(site_model$rate_ag_prior_distr) +
+      beautier::get_distr_n_params(site_model$rate_at_prior_distr) +
+      beautier::get_distr_n_params(site_model$rate_cg_prior_distr) +
+      beautier::get_distr_n_params(site_model$rate_gt_prior_distr)
     )
-  } else if (is_hky_site_model(site_model)) { # nolint beautier function
+  } else if (beautier::is_hky_site_model(site_model)) {
     return(
       gamma_site_model_n_params +
-      get_distr_n_params(site_model$kappa_prior_distr) # nolint beautier function
+      beautier::get_distr_n_params(site_model$kappa_prior_distr)
     )
-  } else if (is_jc69_site_model(site_model)) { # nolint beautier function
+  } else if (beautier::is_jc69_site_model(site_model)) {
     return(
       gamma_site_model_n_params +
       0
     )
   } else {
-    testit::assert(is_tn93_site_model(site_model)) # nolint beautier function
+    testit::assert(beautier::is_tn93_site_model(site_model))
     return(
       gamma_site_model_n_params +
-      get_distr_n_params(site_model$kappa_1_prior_distr) + # nolint beautier function
-      get_distr_n_params(site_model$kappa_2_prior_distr) # nolint beautier function
+      beautier::get_distr_n_params(site_model$kappa_1_prior_distr) +
+      beautier::get_distr_n_params(site_model$kappa_2_prior_distr)
     )
   }
 }
