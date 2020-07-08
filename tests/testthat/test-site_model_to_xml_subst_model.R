cbfabe80fae8f3b902afba747ba57640a4e762c8
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-08 13:45:58 +0200

    'site_model_to_xml_subst_model' tests work on inference model

--- tests/testthat/test-site_model_to_xml_subst_model.R
+++ tests/testthat/test-site_model_to_xml_subst_model.R
@@ -1,7 +1,10 @@
 test_that("use, JC69", {
 
+  inference_model <- create_inference_model(
+    site_model = create_jc69_site_model(id = 123)
+  )
   created <- site_model_to_xml_subst_model(
-    create_jc69_site_model(id = 123)
+    inference_model$site_model
   )
   expected <- "<substModel id=\"JC69.s:123\" spec=\"JukesCantor\"/>"
   expect_equal(created, expected)
@@ -9,8 +12,12 @@ test_that("use, JC69", {
 
 test_that("use, HKY", {
 
-  created <- site_model_to_xml_subst_model(
+  inference_model <- create_inference_model(
     create_hky_site_model(id = 123)
+  )
+
+  created <- site_model_to_xml_subst_model(
+    site_model = inference_model$site_model
   )
   expected <- c(
     "<substModel id=\"hky.s:123\" spec=\"HKY\" kappa=\"@kappa.s:123\">",
@@ -22,8 +29,11 @@ test_that("use, HKY", {
 
 test_that("use, TN93", {
 
+  inference_model <- create_inference_model(
+    site_model = create_tn93_site_model(id = 123)
+  )
   created <- site_model_to_xml_subst_model(
-    create_tn93_site_model(id = 123)
+    site_model = inference_model$site_model
   )
   expected <- c(
     "<substModel id=\"tn93.s:123\" spec=\"TN93\" kappa1=\"@kappa1.s:123\" kappa2=\"@kappa2.s:123\">", # nolint indeed a long line
@@ -35,8 +45,12 @@ test_that("use, TN93", {
 
 test_that("use, GTR", {
 
+  inference_model <- create_inference_model(
+    site_model = create_gtr_site_model(id = 123)
+  )
+
   created <- site_model_to_xml_subst_model(
-    create_gtr_site_model(id = 123)
+    site_model = inference_model$site_model
   )
   expected <- c(
     "<substModel id=\"gtr.s:123\" spec=\"GTR\" rateAC=\"@rateAC.s:123\" rateAG=\"@rateAG.s:123\" rateAT=\"@rateAT.s:123\" rateCG=\"@rateCG.s:123\" rateGT=\"@rateGT.s:123\">", # nolint indeed a long line
