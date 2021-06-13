ed5fb64fab6f449662c87e45d18336f74ce1dcbd
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-13 15:51:22 +0200

    Expose error in tracelog for https://github.com/ropensci/babette/issues/99

--- tests/testthat/test-create_tracelog_xml.R
+++ tests/testthat/test-create_tracelog_xml.R
@@ -89,6 +89,46 @@ test_that("detailed use, v2.6, RLN", {
   expect_equal(sort(created), sort(expected))
 })
 
+
+test_that("RLN + tipdates, v2.6", {
+  skip("https://github.com/ropensci/babette/issues/99, create_tracelog_xml")
+  inference_model <- create_inference_model(
+    tree_prior = create_yule_tree_prior(
+      id = "test_output_0",
+      birth_rate_distr = create_uniform_distr(id = 1)
+    ),
+    clock_model = create_rln_clock_model(
+      id = "test_output_0",
+      ucldstdev_distr = create_gamma_distr(
+        alpha = create_alpha_param(id = 2, value = "0.5396"),
+        beta = create_beta_param(id = 3, value = "0.3819")
+      ),
+      mparam_id = 1
+    ),
+    tipdates_filename = get_beautier_path("test_output_0_tipdates.tsv"),
+    beauti_options = create_beauti_options_v2_6()
+  )
+  created <- create_tracelog_xml(
+    input_filename = get_beautier_path("test_output_0.fas"),
+    inference_model = inference_model
+  )
+  expected <- unindent(
+    remove_empty_lines(
+      extract_xml_section_from_lines(
+        readr::read_lines(get_beautier_path("rln_tipdates_2_6.xml")),
+        section = "logger"
+      )
+    )[1:12]
+  )
+  compare_lines(
+    lines = created,
+    expected = expected,
+    created_lines_filename = "~/created.xml",
+    expected_lines_filename = "~/expected.xml",
+  )
+  expect_true(are_equivalent_xml_lines(created, expected))
+})
+
 test_that("file_name in XML", {
   input_filename <- get_fasta_filename()
   inference_model <- init_inference_model(
