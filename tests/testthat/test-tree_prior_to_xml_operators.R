98ce088882c5eda0e94c0855891af3101aac0d51
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-13 15:36:45 +0200

    Expose error in tree_prior_to_xml_operators

--- tests/testthat/test-tree_prior_to_xml_operators.R
+++ tests/testthat/test-tree_prior_to_xml_operators.R
@@ -35,3 +35,41 @@ test_that("Yule, v2.6.0", {
   )
   expect_true(are_equivalent_xml_lines(created, expected))
 })
+
+test_that("RLN + tipdates, v2.6", {
+  skip("https://github.com/ropensci/babette/issues/99, hiero")
+  inference_model <- create_inference_model(
+    tree_prior = create_yule_tree_prior(
+      id = "test_output_0",
+      birth_rate_distr = create_uniform_distr(id = 1)
+    ),
+    clock_model = create_rln_clock_model(
+      ucldstdev_distr = create_gamma_distr(
+        alpha = create_alpha_param(id = 2, value = "0.5396"),
+        beta = create_beta_param(id = 3, value = "0.3819")
+      ),
+      mparam_id = 1
+    ),
+    tipdates_filename = get_beautier_path("test_output_0_tipdates.tsv"),
+    beauti_options = create_beauti_options_v2_6()
+  )
+  created <- tree_prior_to_xml_operators(
+    inference_model = inference_model
+  )
+  expected <- stringr::str_subset(
+    stringr::str_trim(
+      stringr::str_subset(
+        readr::read_lines(get_beautier_path("rln_tipdates_2_6.xml")),
+        "<operator id="
+      )
+    ),
+    "\\.t:test_output_0"
+  )
+  compare_lines(
+    lines = created,
+    expected = expected,
+    created_lines_filename = "~/created.xml",
+    expected_lines_filename = "~/expected.xml",
+  )
+  expect_true(are_equivalent_xml_lines(created, expected))
+})
