22688bdc3e59ecff48ef979809db15b081c2e473
richelbilderbeek <richel@richelbilderbeek.nl>
2023-03-09 11:35:28 +0100

    Can recreate 'issue_135_no_mrca_no_estimate_beauti.xml'

--- tests/testthat/test-issue_135.R
+++ tests/testthat/test-issue_135.R
@@ -1,56 +1,14 @@
-test_that("1: expose problem: files are different already", {
-  # Verify these two files are equivalent
-  #
-  # * issue_135_no_mrca_no_estimate_beauti.xml
-  # * issue_135_no_mrca_no_estimate_beautier.xml
-  #
-  # They are not, which is part of the problem.
-  # The next test recreates the correct beautier file
+test_that("1: files were different, are the same now", {
+  # The submitted files were not equivalent.
+  # This helped fix beautier.
+  # Now, a created beautier file 'issue_135_no_mrca_no_estimate_beautier.xml'
+  # indeed is correct.
   beauti_file <- beautier::get_beautier_path("issue_135_no_mrca_no_estimate_beauti.xml")
   beautier_file <- beautier::get_beautier_path("issue_135_no_mrca_no_estimate_beautier.xml")
-
-  expect_false(beautier::are_equivalent_xml_files(beauti_file, beautier_file))
+  expect_true(beautier::are_equivalent_xml_files(beauti_file, beautier_file))
 })
 
-test_that("re-create file step by step 2/?: HKY site model", {
-
-  skip("WIP #135")
-  # # Make the inference model match the BEAUti file
-  inference_model <- create_inference_model(
-    site_model = create_hky_site_model(),
-    beauti_options = beautier::create_beauti_options_v2_6(
-      nucleotides_uppercase = TRUE
-    )
-  )
-  inference_model$tree_prior$birth_rate_distr$id <- "1"
-  inference_model$site_model$kappa_prior_distr$m$id <- "1"
-  inference_model$site_model$kappa_prior_distr$s$id <- "2"
-  inference_model$site_model$gamma_site_model$freq_prior_uniform_distr_id <- "3"
-
-  beautier_file <- get_beautier_tempfilename(pattern = "anthus_aco_sub_2_6")
-  create_beast2_input_file_from_model(
-    input_filename = beautier::get_beautier_path("anthus_aco_sub.fas"),
-    output_filename = beautier_file,
-    inference_model = inference_model
-  )
-  # If this passes, this is done!
-  beauti_file <- beautier::get_beautier_path("hky_2_6.xml")
-  expect_true(
-    beautier::are_equivalent_xml_files(
-      beautier_file,
-      beauti_filename
-    )
-  )
-  beautier::compare_lines(
-    lines = readr::read_lines(beautier_file),
-    expected = readr::read_lines(beauti_file),
-    created_lines_filename = "~/created.xml",
-    expected_lines_filename = "~/expected.xml"
-  )
-})
-
-test_that("1: re-created file", {
-  skip("Issue #135, Issue 135")
+test_that("1: can re-create file 'issue_135_no_mrca_no_estimate_beauti.xml'", {
   # Re-create a beautier file that is equivalent to the BEAUti file
   # The BEAUti file is correct:
   # issue_135_no_mrca_no_estimate_beauti.xml
@@ -58,8 +16,9 @@ test_that("1: re-created file", {
   # This is a unique file, delivered by the user
   beauti_file <- beautier::get_beautier_path("issue_135_no_mrca_no_estimate_beauti.xml")
 
-  file.copy(beauti_file, "~/issue_135_no_mrca_no_estimate_beauti.xml")
-  beautier_file <- "~/issue_135_no_mrca_no_estimate_beautier.xml"
+  # file.copy(beauti_file, "~/issue_135_no_mrca_no_estimate_beauti.xml")
+  #beautier_file <- "~/issue_135_no_mrca_no_estimate_beautier.xml"
+  beautier_file <- get_beautier_tempfilename()
 
   #without mrca prior, single value at clock rate
   fasta_filename <- beautier::get_beautier_path("anthus_aco_sub.fas")
@@ -76,6 +35,7 @@ test_that("1: re-created file", {
   inference_model$tree_prior$birth_rate_distr$id <- "1"
   inference_model$site_model$kappa_prior_distr$m$id <- "1"
   inference_model$site_model$kappa_prior_distr$s$id <- "2"
+  inference_model$site_model$gamma_site_model$freq_prior_uniform_distr_id <- "3"
 
   create_beast2_input_file_from_model(
     input_filename = fasta_filename,
@@ -85,25 +45,16 @@ test_that("1: re-created file", {
   # If this passes, this is done!
   expect_true(beautier::are_equivalent_xml_files(beauti_file, beautier_file))
 
-
   beauti_text <- readr::read_lines(beauti_file)
   beautier_text <- readr::read_lines(beautier_file)
-  beautier::compare_lines(
-    lines = beautier_text,
-    expected = beauti_text,
-    created_lines_filename = "~/created.xml",
-    expected_lines_filename = "~/expected.xml"
-  )
-
-  # Third fix, only present in the issue_135_ files
-  freq_param_regex <- "<operator id=.YuleBirthRateScaler.t:anthus_aco_sub. spec=.ScaleOperator. parameter=.@birthRate.t:anthus_aco_sub. weight=.3.0./>"
-  expect_equal(1, sum(stringr::str_count(beauti_text, freq_param_regex)))
-  expect_equal(1, sum(stringr::str_count(beautier_text, freq_param_regex)))
-
-  # Fourth fix, only present in the issue_135_ files
-  freq_prior_regex <- "<prior id=.FrequenciesPrior.s:anthus_aco_sub. name=.distribution. x=.@freqParameter.s:anthus_aco_sub.>"
-  expect_equal(1, sum(stringr::str_count(beauti_text, freq_prior_regex)))
-  expect_equal(1, sum(stringr::str_count(beautier_text, freq_prior_regex)))
+  if (1 == 2) {
+    beautier::compare_lines(
+      lines = beautier_text,
+      expected = beauti_text,
+      created_lines_filename = "~/created.xml",
+      expected_lines_filename = "~/expected.xml"
+    )
+  }
 
   # First fix, works!
   kappa_param_regex <- "<parameter id=.kappa.s:anthus_aco_sub. spec=.parameter.RealParameter. lower=.0.0. name=.stateNode.>2.0</parameter>"
@@ -114,6 +65,18 @@ test_that("1: re-created file", {
   freq_param_regex <- "<parameter id=.freqParameter.s:anthus_aco_sub. spec=.parameter.RealParameter. dimension=.4. lower=.0.0. name=.stateNode. upper=.1.0.>0.25</parameter>"
   expect_equal(1, sum(stringr::str_count(beauti_text, freq_param_regex)))
   expect_equal(1, sum(stringr::str_count(beautier_text, freq_param_regex)))
+
+  # Third fix, works
+  freq_param_regex <- "<operator id=.YuleBirthRateScaler.t:anthus_aco_sub. spec=.ScaleOperator. parameter=.@birthRate.t:anthus_aco_sub. weight=.3.0./>"
+  expect_equal(1, sum(stringr::str_count(beauti_text, freq_param_regex)))
+  expect_equal(1, sum(stringr::str_count(beautier_text, freq_param_regex)))
+
+  # Fourth fix, works
+  freq_prior_regex <- "<prior id=.FrequenciesPrior.s:anthus_aco_sub. name=.distribution. x=.@freqParameter.s:anthus_aco_sub.>"
+  expect_equal(1, sum(stringr::str_count(beauti_text, freq_prior_regex)))
+  expect_equal(1, sum(stringr::str_count(beautier_text, freq_prior_regex)))
+
+  beautier::remove_beautier_folder()
 })
 
 test_that("use", {
