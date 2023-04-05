171c4abd403aedd32ac545b760f0d2d6ef4f33cd
richelbilderbeek <richel@richelbilderbeek.nl>
2023-04-05 09:10:19 +0200

    Fix #137

--- tests/testthat/test-issue_137.R
+++ tests/testthat/test-issue_137.R
@@ -1,7 +1,5 @@
 test_that("1: can re-create file 'issue_137_lognormal_estimate_beauti.xml'", {
 
-  skip("#137")
-
   # Delivered by the user
   beauti_file <- beautier::get_beautier_path(
     "issue_137_lognormal_estimate_beauti.xml"
@@ -14,16 +12,18 @@ test_that("1: can re-create file 'issue_137_lognormal_estimate_beauti.xml'", {
   )
   clock_rate_distr <- beautier::create_log_normal_distr(
     m = beautier::create_m_param(value = "-5.73"),
+    s = beautier::create_s_param(value = "0.5", lower = "0.0", upper = "5.0"),
     value = "5.0"
+  )
+  clock_model <- beautier::create_strict_clock_model(
+    id = "anthus_aco_sub",
+    clock_rate_param = clock_rate,
+    clock_rate_distr = clock_rate_distr
   )
 
   inference_model <- create_inference_model(
     site_model = beautier::create_hky_site_model(),
-    clock_model = beautier::create_strict_clock_model(
-      id = NA,
-      clock_rate_param = clock_rate,
-      clock_rate_distr = clock_rate_distr
-    ),
+    clock_model = clock_model,
     tree_prior = create_yule_tree_prior(),
     beauti_options = beautier::create_beauti_options_v2_6(
       nucleotides_uppercase = TRUE,
@@ -36,7 +36,9 @@ test_that("1: can re-create file 'issue_137_lognormal_estimate_beauti.xml'", {
   inference_model$site_model$kappa_prior_distr$m$id <- "1"
   inference_model$site_model$kappa_prior_distr$s$id <- "2"
   inference_model$site_model$gamma_site_model$freq_prior_uniform_distr_id <- "3"
-  inference_model$clock_model$clock_rate_distr$id <- "0"
+  inference_model$clock_model$clock_rate_distr$id <- "1"
+  inference_model$clock_model$clock_rate_distr$m$id <- "3"
+  inference_model$clock_model$clock_rate_distr$s$id <- "4"
 
   create_beast2_input_file_from_model(
     input_filename = fasta_filename,
@@ -44,30 +46,5 @@ test_that("1: can re-create file 'issue_137_lognormal_estimate_beauti.xml'", {
     inference_model = inference_model
   )
   expect_true(beautier::are_equivalent_xml_files(beauti_file, beautier_file))
-
-  beautier::compare_lines(
-    lines = readr::read_lines(beautier_file),
-    expected = readr::read_lines(beauti_file),
-    created_lines_filename = "~/created.xml",
-    expected_lines_filename = "~/expected.xml",
-  )
-
-  expect_equal(
-    1,
-    length(
-      stringr::str_subset(
-        readr::read_lines(beautier_file),
-        "<parameter id=.clockRate.c:anthus_aco_sub. spec=.parameter.RealParameter. name=.stateNode.>0.003536</parameter>"
-      )
-    )
-  )
-  # <prior id="ClockPrior.c:anthus_aco_sub" name="distribution" x="@clockRate.c:anthus_aco_sub">
-  #   <LogNormal id="LogNormalDistributionModel.1" name="distr">
-  #     <parameter id="RealParameter.3" spec="parameter.RealParameter" estimate="false" name="M">-5.73</parameter>
-  #     <parameter id="RealParameter.4" spec="parameter.RealParameter" estimate="false" lower="0.0" name="S" upper="5.0">0.5</parameter>
-  #   </LogNormal>
-  # </prior>
-
-
   beautier::remove_beautier_folder()
 })
