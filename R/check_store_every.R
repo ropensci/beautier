b3f36068c49ecfc5bde490357c34fed18cb7bc76
olivroy <olivierroy71@hotmail.com>
2023-10-26 11:49:36 -0400

    rm testthat from R

--- R/check_store_every.R
+++ R/check_store_every.R
@@ -6,7 +6,6 @@
 check_store_every <- function(store_every) {
   check_number_whole(store_every, allow_na = TRUE, min = -1)
   if (is.na(store_every)) return()
-  testthat::expect_true(beautier::is_one_int(store_every))
 
   if (store_every == 0) {
     stop(
