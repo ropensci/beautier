dfd16e42ccf66f1b0bfe8162b38134c5fe825181
richelbilderbeek <richel@richelbilderbeek.nl>
2023-12-30 14:45:17 +0100

    Start #142

---
+++ tests/testthat/test-issue_142.R
@@ -0,0 +1,5 @@
+test_that("use", {
+  # beautier Issue 142, https://github.com/ropensci/beautier/issues/142
+  # For babette Issue 106, https://github.com/ropensci/babette/issues/106
+  expect_equal(2 * 2, 4)
+})
