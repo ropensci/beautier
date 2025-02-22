a9b67374d8e8b8b08ab141a47da5fedd774bebfc
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-02-22 11:08:47 +0100

    Can reproduce file by user, progress #108

--- tests/testthat/test-babette_issue_108.R
+++ tests/testthat/test-babette_issue_108.R
@@ -45,11 +45,10 @@ test_that("tipdates file must be used in the created file", {
     )
   )
   expect_true(file.exists(output_filename))
+
+  # Tipdates end up in output file
   beast2_xml_lines <- readr::read_lines(output_filename)
   expected_beast2_xml_lines <- readr::read_lines(expected_output_filename)
-
-
-
   expect_true(
     length(
       stringr::str_subset(
@@ -59,9 +58,93 @@ test_that("tipdates file must be used in the created file", {
     ) > 0
   )
 
-  # readr::write_lines(beast2_xml_lines, "~/created.xml")
-  # readr::write_lines(expected_beast2_xml_lines, "~/expected.xml")
-  # beastier::are_beast2_input_lines_deep(beast2_xml_lines)
+  # Reproduce file
+  readr::write_lines(beast2_xml_lines, "~/created.xml")
+  readr::write_lines(expected_beast2_xml_lines, "~/expected.xml")
+  beastier::are_beast2_input_lines_deep(beast2_xml_lines)
+
+  # First step of fixing #108
+  beautier::are_equivalent_xml_files(
+    output_filename,
+    expected_output_filename
+  )
+
+  remove_beautier_folder()
+})
+
+test_that("Reproduce file", {
+
+  fasta_filename_beautier <- get_beautier_path("babette_issue_108.fasta")
+  # Copy to have 1996-2000 in the data id
+  fasta_filename <- file.path(
+    rappdirs::user_cache_dir(appname = "beautier"),
+    "1996-2000.fasta"
+  )
+  file.copy(from = fasta_filename_beautier, to = fasta_filename)
+
+
+  tipdates_filename <- get_beautier_path("babette_issue_108_tipdates.txt")
+  output_filename <- get_beautier_tempfilename()
+  expected_output_filename <- get_beautier_path(
+    "babette_issue_108_expected_20250203.xml"
+  )
+  # Shallow check works
+  expect_true(
+    beastier::are_beast2_input_lines(
+      readr::read_lines(expected_output_filename)
+    )
+  )
+  if (1 == 2) {
+    # Deep check fails
+    # Fix https://github.com/ropensci/beastier/issues/72
+    beastier::are_beast2_input_lines_deep(
+      readr::read_lines(expected_output_filename), verbose = TRUE
+
+    )
+  }
+
+  create_beast2_input_file(
+    input_filename = fasta_filename,
+    output_filename = output_filename,
+    tipdates_filename = tipdates_filename,
+    clock_model = beautier::create_rln_clock_model(mean_clock_rate = "1.0"),
+    site_model = beautier::create_hky_site_model(),
+    mcmc = beautier::create_mcmc(),
+    tree_prior = beautier::create_cbs_tree_prior(),
+    beauti_options = beautier::create_beauti_options(beast2_version = "2.5")
+  )
+
+  # Reproduce file
+  beast2_xml_lines <- readr::read_lines(output_filename)
+  expected_beast2_xml_lines <- readr::read_lines(expected_output_filename)
+  readr::write_lines(beast2_xml_lines, "~/created.xml")
+  readr::write_lines(expected_beast2_xml_lines, "~/expected.xml")
+
+  # First step of fixing #108
+  beautier::are_equivalent_xml_lines(
+    beast2_xml_lines,
+    expected_beast2_xml_lines
+  )
+
+
+  if (1 == 2) {
+    # Part of user-submitted code, not needed to
+    # reproduce BEAST2 XML input file
+    assignInNamespace(
+      "get_beast2_main_class_name",
+      function() {
+          "beast.app.beastapp.BeastMain"
+      },
+      ns = "beastier"
+    )
+    run_beast2(
+      input_filename = glue("{start}-{end}.xml"),
+      output_state_filename = create_temp_state_filename(),
+      use_beagle = TRUE,
+      beast2_path = "/home/zhulab/.beast/2.5/BEAST/lib/beast.jar",
+      verbose = T
+    )
+  }
 
   remove_beautier_folder()
 })
